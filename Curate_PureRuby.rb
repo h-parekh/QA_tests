e0a89a9ab06ce447b732278ad1b6d90eb2fee147
hparekh <hparekh@nd.edu>
2016-10-03 15:04:15 -0400

    Adding username/password as input fields

--- Curate_PureRuby.rb
+++ Curate_PureRuby.rb
@@ -3,6 +3,8 @@ require 'capybara/rspec'
 require 'rspec/autorun'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
+#HighLine provides a robust system for requesting data from a user
+require 'highline/import'
 
 Capybara.run_server = false
 Capybara.current_driver = :poltergeist
@@ -21,76 +23,80 @@ RSpec.configure do |config|
 end
 
 feature 'User login', :js => true do
-#     scenario 'Test 1: Login to Curate with correct credentials' do
-#         visit '/'
-#         expect(page).to have_link 'Log In'
-#
-# #Using the same has_link assertion with options
-#         #expect(page).to have_link 'Log In', href: '/users/sign_in'
-#
-# #To print the html:
-#         #puts(page.html)
-#         #print page.html
-#
-# #Take screenshot of the page, save_and_open_screenshot needs launchy gem
-#         #page.save_screenshot('curate_test_screenshot.png')
-#         #page.save_and_open_screenshot('curate_test_screenshot2.png')
-#
-#         click_on('Log In')
-#         if expect(page).to have_field 'username', type: 'text'
-#           print "First login, enter username and password\n"
-#           fill_in('username', with: 'username')
-#         else
-#           expect(page).to have_field 'password', type: 'password'
-#           print "Trusted device, needs only password\n"
-#         end
-#
-#         print "Filling in password next\n"
-#         fill_in('password', with: 'password')
-#
-#         print "Logging in with username and password fields\n"
-#         puts current_url
-#         click_on('LOGIN')
-#
-# #Accessing a specific xpath and running a validation
-#         within('.mfa-text-wrapper') do
-#           expect(page).to have_content 'Send'
-#         end
-#
-#
-#         print("The next click should send a push to Android (2576)\n")
-#         expect(page).to have_button 'LOGIN'
-#
-#         puts current_url
-#         click_on('LOGIN')
-#         #Use below syntax if you want to wait
-#         #click_on('LOGIN', wait: 60)
-#
-#         # if expect(page).to have_content('Incorrect NetID or password.')
-#         #   print "Succesfull negative login test"
-#         # elsif expect(page).to have_content 'Android (2576)'
-#         #   page.save_screenshot('curate_test_screenshot.png')
-#         #   print "Succesfull postive login test"
-#         # end
-#     end
+    scenario 'Test 1: Login to Curate with correct credentials' do
+        visit '/'
+        expect(page).to have_link 'Log In'
 
-    scenario 'Test 1: Login to Curate with incorrect credentials' do
+#Using the same has_link assertion with options
+        #expect(page).to have_link 'Log In', href: '/users/sign_in'
+
+#To print the html:
+        #puts(page.html)
+        #print page.html
+
+#Take screenshot of the page, save_and_open_screenshot needs launchy gem
+        #page.save_screenshot('curate_test_screenshot.png')
+        #page.save_and_open_screenshot('curate_test_screenshot2.png')
+
+        click_on('Log In')
+        if expect(page).to have_field 'username', type: 'text'
+          #getting username using highline gem syntax
+          p_username = ask("Enter valid username:  ") { |q| q.echo = true }
+          fill_in('username', with: p_username)
+        else
+          expect(page).to have_field 'password', type: 'password'
+          print "Trusted device, needs only password\n"
+        end
+
+        #getting password using highline gem syntax
+        p_password = ask("Enter valid password:  ") { |q| q.echo = "*" }
+        fill_in('password', with: p_password)
+
+        print "Logging in with username and password fields\n"
+        puts current_url
+        click_on('LOGIN')
+
+#Accessing a specific xpath and running a validation
+        within('.mfa-text-wrapper') do
+          expect(page).to have_content 'Send'
+        end
+
+
+        print("The next click should send a push to Android (2576)\n")
+        expect(page).to have_button 'LOGIN'
+
+        puts current_url
+        click_on('LOGIN')
+        #Use below syntax if you want to wait
+        #click_on('LOGIN', wait: 60)
+
+        # if expect(page).to have_content('Incorrect NetID or password.')
+        #   print "Succesfull negative login test"
+        # elsif expect(page).to have_content 'Android (2576)'
+        #   page.save_screenshot('curate_test_screenshot.png')
+        #   print "Succesfull postive login test"
+        # end
+    end
+
+    scenario 'Test 2: Login to Curate with incorrect credentials' do
                 visit '/'
                 expect(page).to have_link 'Log In'
                 click_on('Log In')
                 if expect(page).to have_field 'username', type: 'text'
-                  print "First login, enter username and password\n"
-                  fill_in('username', with: 'username')
+                  #getting username using highline gem syntax
+                  p_username = ask("Enter INVALID username:  ") { |q| q.echo = true }
+                  fill_in('username', with: p_username)
                 else
                   expect(page).to have_field 'password', type: 'password'
                   print "Trusted device, needs only password\n"
                 end
 
-                print "Filling in password next\n"
-                fill_in('password', with: 'password')
+                #getting password using highline gem syntax
+                p_password = ask("Enter INVALID password:  ") { |q| q.echo = "*" }
+                fill_in('password', with: p_password)
 
                 print "Logging in with username and password fields\n"
-                
+
                 click_on('LOGIN')
                 if expect(page).to have_content 'Incorrect NetID or password.'
                   print "Successfully tested login with invalid creds"
