e46051bbbc72d4c8d22e512e25b48fe6991b0722
hparekh <hparekh@nd.edu>
2016-10-03 13:44:19 -0400

    Adding multiple scenarios in same feature test

--- Curate_PureRuby.rb
+++ Curate_PureRuby.rb
@@ -10,6 +10,8 @@ Capybara.current_driver = :poltergeist
 
 #If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
 Capybara.javascript_driver = :poltergeist
+
+#Enable this to work with hidden fields
 #Capybara.ignore_hidden_elements = false
 
 Capybara.app_host = 'https://curatepprd.library.nd.edu/'
@@ -18,56 +20,81 @@ RSpec.configure do |config|
     config.include Capybara::DSL
 end
 
-feature 'Browsing', :js => true do
-    scenario 'Login to Curate' do
-        visit '/'
-        expect(page).to have_link 'Log In'
-        #Using the same has_link assertion with options
-        #expect(page).to have_link 'Log In', href: '/users/sign_in'
-        #To print the html:
-        #puts(page.html)
-        # print page.html
+feature 'User login', :js => true do
+#     scenario 'Test 1: Login to Curate with correct credentials' do
+#         visit '/'
+#         expect(page).to have_link 'Log In'
+#
+# #Using the same has_link assertion with options
+#         #expect(page).to have_link 'Log In', href: '/users/sign_in'
+#
+# #To print the html:
+#         #puts(page.html)
+#         #print page.html
+#
+# #Take screenshot of the page, save_and_open_screenshot needs launchy gem
+#         #page.save_screenshot('curate_test_screenshot.png')
+#         #page.save_and_open_screenshot('curate_test_screenshot2.png')
+#
+#         click_on('Log In')
+#         if expect(page).to have_field 'username', type: 'text'
+#           print "First login, enter username and password\n"
+#           fill_in('username', with: 'username')
+#         else
+#           expect(page).to have_field 'password', type: 'password'
+#           print "Trusted device, needs only password\n"
+#         end
+#
+#         print "Filling in password next\n"
+#         fill_in('password', with: 'password')
+#
+#         print "Logging in with username and password fields\n"
+#         puts current_url
+#         click_on('LOGIN')
+#
+# #Accessing a specific xpath and running a validation
+#         within('.mfa-text-wrapper') do
+#           expect(page).to have_content 'Send'
+#         end
+#
+#
+#         print("The next click should send a push to Android (2576)\n")
+#         expect(page).to have_button 'LOGIN'
+#
+#         puts current_url
+#         click_on('LOGIN')
+#         #Use below syntax if you want to wait
+#         #click_on('LOGIN', wait: 60)
+#
+#         # if expect(page).to have_content('Incorrect NetID or password.')
+#         #   print "Succesfull negative login test"
+#         # elsif expect(page).to have_content 'Android (2576)'
+#         #   page.save_screenshot('curate_test_screenshot.png')
+#         #   print "Succesfull postive login test"
+#         # end
+#     end
 
-        #Take screenshot of the page, save_and_open_screenshot needs launchy gem
-        #page.save_screenshot('curate_test_screenshot.png')
-        #page.save_and_open_screenshot('curate_test_screenshot2.png')
+    scenario 'Test 1: Login to Curate with incorrect credentials' do
+                visit '/'
+                expect(page).to have_link 'Log In'
+                click_on('Log In')
+                if expect(page).to have_field 'username', type: 'text'
+                  print "First login, enter username and password\n"
+                  fill_in('username', with: 'username')
+                else
+                  expect(page).to have_field 'password', type: 'password'
+                  print "Trusted device, needs only password\n"
+                end
 
-        click_on('Log In')
-        if expect(page).to have_field 'username', type: 'text'
-          print "First login, enter username and password\n"
-          fill_in('username', with: 'username')
-        else
-          expect(page).to have_field 'password', type: 'password'
-          print "Trusted device, needs only password\n"
-        end
+                print "Filling in password next\n"
+                fill_in('password', with: 'password')
 
-        print "Filling in password next\n"
-        fill_in('password', with: 'password')
-
-        print "Logging in with username and password fields\n"
-        puts current_url
-        click_on('LOGIN')
-
-        within('.mfa-text-wrapper') do
-          expect(page).to have_content 'Send'
-        end
-
-
-        print("The next click should send a push to Android (2576)\n")
-        expect(page).to have_button 'LOGIN'
-
-        puts current_url
-        click_on('LOGIN')
-        #Use below syntax if you want to wait
-        #click_on('LOGIN', wait: 60)
-
-        # if expect(page).to have_content('Incorrect NetID or password.')
-        #   print "Succesfull negative login test"
-        # elsif expect(page).to have_content 'Android (2576)'
-        #   page.save_screenshot('curate_test_screenshot.png')
-        #   print "Succesfull postive login test"
-        # end
-
-
+                print "Logging in with username and password fields\n"
+                
+                click_on('LOGIN')
+                if expect(page).to have_content 'Incorrect NetID or password.'
+                  print "Successfully tested login with invalid creds"
+                end
     end
+
 end
