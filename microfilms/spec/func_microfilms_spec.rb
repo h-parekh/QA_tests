d8446cbc1bd1f878e166c9c11f2e9c4c9b924001
hparekh <hparekh@nd.edu>
2016-10-20 15:01:59 -0400

    adding microfilms functional test

--- microfilms/spec/func_microfilms_spec.rb
+++ microfilms/spec/func_microfilms_spec.rb
@@ -18,14 +18,14 @@ Capybara.javascript_driver = :poltergeist
 #Enable this to work with hidden fields
 #Capybara.ignore_hidden_elements = false
 
-cnf = YAML::load_file(File.join(__dir__, 'config.yml'))
-trigger = YAML::load_file(File.join(__dir__, 'trigger.yml'))
+cnf = YAML::load_file(File.join(__dir__, 'microfilms_config.yml'))
+trigger = YAML::load_file(File.join(__dir__, 'microfilms_trigger.yml'))
 target_env = trigger['target']
 
 if target_env.nil? || target_env.to_s == ''
-  target_env = ask("Enter target env, options are staging6, staging8, staging9, pprd or prod:  ") { |q| q.echo = true }
+  target_env = ask("Enter target env, options are pprd or prod:  ") { |q| q.echo = true }
 end
-
+print cnf[target_env]
 Capybara.app_host = cnf[target_env]
 
 #Gives access to the capybara methods
@@ -34,104 +34,21 @@ RSpec.configure do |config|
 end
 
 feature 'User Browsing', :js => true do
-#     scenario 'Test 1: Login to Curate with correct credentials' do
-#         visit '/'
-#         expect(page).to have_link 'Log In'
-#
-# #Using the same has_link assertion with options
-#         #expect(page).to have_link 'Log In', href: '/users/sign_in'
-#
-# #To print the html:
-#         #puts(page.html)
-#         #print page.html
-#
-# #Take screenshot of the page, save_and_open_screenshot needs launchy gem
-#         #page.save_screenshot('curate_test_screenshot.png')
-#         #page.save_and_open_screenshot('curate_test_screenshot2.png')
-#
-#         click_on('Log In')
-#         if expect(page).to have_field 'username', type: 'text'
-#           #getting username using highline gem syntax
-#           p_username = ask("Enter valid username:  ") { |q| q.echo = true }
-#           fill_in('username', with: p_username)
-#         else
-#           expect(page).to have_field 'password', type: 'password'
-#           print "Trusted device, needs only password\n"
-#         end
-#
-#         #getting password using highline gem syntax
-#         p_password = ask("Enter valid password:  ") { |q| q.echo = "*" }
-#         fill_in('password', with: p_password)
-#
-#         print "Logging in with username and password fields\n"
-#         puts current_url
-#         click_on('LOGIN')
-#
-# #Accessing a specific xpath and running a validation
-#         within('.mfa-text-wrapper') do
-#           expect(page).to have_content 'Send'
-#         end
-#
-#
-#         print("The next click should send a push to Android\n")
-#         expect(page).to have_button 'LOGIN'
-#
-#         puts current_url
-#         click_on('LOGIN')
-#         #Use below syntax if you want to wait
-#         #click_on('LOGIN', wait: 60)
-#
-#         # if expect(page).to have_content('Incorrect NetID or password.')
-#         #   print "Succesfull negative login test"
-#         # elsif expect(page).to have_content 'Android (2576)'
-#         #   page.save_screenshot('curate_test_screenshot.png')
-#         #   print "Succesfull postive login test"
-#         # end
-#     end
-#
-#     scenario 'Test 2: Login to Curate with incorrect credentials' do
-#                 visit '/'
-#                 expect(page).to have_link 'Log In'
-#                 click_on('Log In')
-#                 if expect(page).to have_field 'username', type: 'text'
-#                   #getting username using highline gem syntax
-#                   p_username = ask("Enter INVALID username:  ") { |q| q.echo = true }
-#                   fill_in('username', with: p_username)
-#                 else
-#                   expect(page).to have_field 'password', type: 'password'
-#                   print "Trusted device, needs only password\n"
-#                 end
-#
-#                 #getting password using highline gem syntax
-#                 p_password = ask("Enter INVALID password:  ") { |q| q.echo = "*" }
-#                 fill_in('password', with: p_password)
-#
-#                 print "Logging in with INVALID username and password fields\n"
-#
-#                 click_on('LOGIN')
-#                 if expect(page).to have_content 'Incorrect NetID or password.'
-#                   print "Successfully tested login with invalid creds\n"
-#                 end
-#     end
-
-
-    scenario 'Test 3: Enter text in search box' do
+    scenario 'Test 1: Enter text in search box' do
       visit '/'
       puts current_url
-      within('.homepage-search') do
-        expect(page).to have_field 'catalog_search', type: 'search'
-        expect(page).to have_button 'keyword-search-submit'
-        fill_in('catalog_search', with: '')
-        click_on('keyword-search-submit')
-        #page.save_screenshot('curate_searchBox.png')
+      within('.search') do
+        expect(page).to have_field 'q', type: 'text'
+        expect(page).to have_button 'search'
+        fill_in('q', with: 'Roman')
+        click_on('search')
+        page.save_screenshot('microfilms_searchBox.png')
         print "Clicked search\n"
       end
 
-      # expect(page).to have_text('Filter by:')
-      # page.save_screenshot('curate_searchBox2.png')
-      within('main') do
-        expect(page).to have_text('Filter by:')
+      within('#appliedParams') do
+          expect(page).to have_link 'startOverLink'
       end
-    end
 
+    end
 end
