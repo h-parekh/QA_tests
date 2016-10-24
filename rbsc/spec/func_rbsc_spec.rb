c882fdec4529fe72ff51c654691b6d1882a5ad12
hparekh <hparekh@nd.edu>
2016-10-24 10:58:54 -0400

    Fleshing out rbsc func test

---
+++ rbsc/spec/func_rbsc_spec.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+#use rspec/autorun only when you need to absolutely run *.rb files using ruby command
+#require 'rspec/autorun'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+#HighLine provides a robust system for requesting data from a user
+require 'highline/import'
+require 'yaml'
+
+Capybara.run_server = false
+Capybara.current_driver = :poltergeist
+#Capybara.default_max_wait_time = 10 #This sets wait time globally
+
+#If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
+Capybara.javascript_driver = :poltergeist
+
+#Enable this to work with hidden fields
+#Capybara.ignore_hidden_elements = false
+
+app_name = 'rbsc'
+config_file = app_name + '_config.yml'
+trigger_file = app_name + '_trigger.yml'
+
+cnf = YAML::load_file(File.join(__dir__, config_file))
+trigger = YAML::load_file(File.join(__dir__, trigger_file))
+target_env = trigger['target']
+if target_env.nil? || target_env.to_s == ''
+  target_env = ask("Enter target env, options are pprd or prod:  ") { |q| q.echo = true }
+end
+
+Capybara.app_host = cnf[target_env]
+
+#Gives access to the capybara methods
+RSpec.configure do |config|
+    config.include Capybara::DSL
+end
+
+js_errors: false
+
+feature 'User Browsing', :js => true do
+    scenario 'Load homepage' do
+      visit '/'
+      puts current_url
+      within('.nav') do
+        expect(page).to have_text 'http://rarebooks.library.nd.edu/'
+      end
+    end
+
+end
