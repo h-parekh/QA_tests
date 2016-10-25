a3be764b479c73a656ab8aa3efcad301b2e18cfc
hparekh <hparekh@nd.edu>
2016-10-25 15:43:30 -0400

    re-arranging files to accomodate spec_helper

--- rbsc/func_rbsc_spec.rb
+++ rbsc/func_rbsc_spec.rb
@@ -6,7 +6,8 @@ require 'capybara/dsl'
 require 'capybara/poltergeist'
 #HighLine provides a robust system for requesting data from a user
 require 'highline/import'
-require 'yaml'
+#require 'yaml'
+require 'spec_helper'
 
 Capybara.run_server = false
 Capybara.current_driver = :poltergeist
@@ -39,15 +40,13 @@ end
 
 feature 'User Browsing', :js => true do
     scenario 'Load homepage' do
-#      rescue Capybara::Poltergeist::JavascriptError
+
+      #Setting js_error false will suppress errors coming back from the site and let the tests finish
       page.driver.browser.js_errors = false
       visit '/'
       puts current_url
-
       within('#main-menu') do
-
         expect(page).to have_css 'li', count: 6
       end
     end
-
 end
