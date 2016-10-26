376a9b669791a3f2d1c1e129d32f53b3b2c2635f
hparekh <hparekh@nd.edu>
2016-10-26 16:09:36 -0400

    Moving rbsc code to spec_helper

--- rbsc/func_rbsc_spec.rb
+++ rbsc/func_rbsc_spec.rb
@@ -1,27 +1,7 @@
 require 'spec_helper'
 
-app_name = 'rbsc'
-config_file = app_name + '_config.yml'
-trigger_file = app_name + '_trigger.yml'
-
-cnf = YAML::load_file(File.join(__dir__, config_file))
-trigger = YAML::load_file(File.join(__dir__, trigger_file))
-target_env = trigger['target']
-if target_env.nil? || target_env.to_s == ''
-  target_env = ask("Enter target env, options are pprd or prod:  ") { |q| q.echo = true }
-end
-
-Capybara.app_host = cnf[target_env]
-
-
-#Gives access to the capybara methods
-RSpec.configure do |config|
-    config.include Capybara::DSL
-end
-
 feature 'User Browsing', :js => true do
     scenario 'Load homepage' do
-
       #Setting js_error false will suppress errors coming back from the site and let the tests finish
       page.driver.browser.js_errors = false
       visit '/'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,21 @@ Capybara.javascript_driver = :poltergeist
 
 #Enable this to work with hidden fields
 #Capybara.ignore_hidden_elements = false
+app_name = 'rbsc'
+config_file = app_name + '_config.yml'
+trigger_file = app_name + '_trigger.yml'
+
+cnf = YAML::load_file(File.expand_path("../" + app_name + "/" + config_file, __dir__))
+trigger = YAML::load_file(File.expand_path("../" + app_name + "/" + trigger_file, __dir__))
+target_env = trigger['target']
+
+if target_env.nil? || target_env.to_s == ''
+  target_env = ask("Enter target env, options are pprd or prod:  ") { |q| q.echo = true }
+end
+
+Capybara.app_host = cnf[target_env]
+
+#Gives access to the capybara methods
+RSpec.configure do |config|
+    config.include Capybara::DSL
+end
