193541c644e35cbb3a584a28557d0a6aee80a7ab
hparekh <hparekh@nd.edu>
2016-10-26 15:21:15 -0400

    Cleaning up additional beehive files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,19 @@
+require 'rubygems'
+#use rspec/autorun only when you need to absolutely run *.rb files using ruby command
+#require 'rspec/autorun'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+#HighLine provides a robust system for requesting data from a user
+require 'highline/import'
 require 'yaml'
+
+Capybara.run_server = false
+Capybara.current_driver = :poltergeist
+#Capybara.default_max_wait_time = 10 #This sets wait time globally
+
+#If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
+Capybara.javascript_driver = :poltergeist
+
+#Enable this to work with hidden fields
+#Capybara.ignore_hidden_elements = false
