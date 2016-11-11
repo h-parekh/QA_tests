e645614d0f138cb0792ef2f01c15deb387fee05f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-11-11 14:07:36 -0500

    Beginning rearrangement of directories

--- spec/microfilms/func_microfilms_spec.rb
+++ spec/microfilms/func_microfilms_spec.rb
@@ -37,6 +37,7 @@ feature 'User Browsing', :js => true do
     scenario 'Test 1: Enter text in search box' do
       visit '/'
       puts current_url
+
       within('.search') do
         expect(page).to have_field 'q', type: 'text'
         expect(page).to have_button 'search'
