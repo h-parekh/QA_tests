3a209ab4aa9166cc5f4291609fafc4418902f3ce
hparekh <hparekh@nd.edu>
2016-11-14 14:53:15 -0500

    Refactoring curate test

--- spec/curate/functional/func_curate_spec.rb
+++ spec/curate/functional/func_curate_spec.rb
@@ -1,37 +1,4 @@
-require 'rubygems'
-#use rspec/autorun only when you need to absolutely run *.rb files using ruby command
-#require 'rspec/autorun'
-require 'capybara/rspec'
-require 'capybara/dsl'
-require 'capybara/poltergeist'
-#HighLine provides a robust system for requesting data from a user
-require 'highline/import'
-require 'yaml'
-
-Capybara.run_server = false
-Capybara.current_driver = :poltergeist
-#Capybara.default_max_wait_time = 10 #This sets wait time globally
-
-#If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
-Capybara.javascript_driver = :poltergeist
-
-#Enable this to work with hidden fields
-#Capybara.ignore_hidden_elements = false
-
-cnf = YAML::load_file(File.join(__dir__, 'curate_config.yml'))
-trigger = YAML::load_file(File.join(__dir__, 'curate_trigger.yml'))
-target_env = trigger['target']
-
-if target_env.nil? || target_env.to_s == ''
-  target_env = ask("Enter target env, options are staging6, staging8, staging9, pprd or prod:  ") { |q| q.echo = true }
-end
-
-Capybara.app_host = cnf[target_env]
-
-#Gives access to the capybara methods
-RSpec.configure do |config|
-    config.include Capybara::DSL
-end
+require 'curate/curate_spec_helper'
 
 feature 'User Browsing', :js => true do
 #     scenario 'Test 1: Login to Curate with correct credentials' do
