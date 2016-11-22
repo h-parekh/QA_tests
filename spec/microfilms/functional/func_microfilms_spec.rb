d24315d2d77522d18ae5b975fe9f558177b052ad
Banurekha <banurekha.l@gmail.com>
2016-11-22 14:28:59 -0500

    cleaning up all rubocop offenses

--- spec/microfilms/functional/func_microfilms_spec.rb
+++ spec/microfilms/functional/func_microfilms_spec.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
 require 'microfilms/microfilms_spec_helper'
 
-feature 'User Browsing', :js => true do
-    scenario 'Test 1: Enter text in search box' do
-      visit '/'
-      puts current_url
+feature 'User Browsing', js: true do
+  scenario 'Test 1: Enter text in search box' do
+    visit '/'
+    puts current_url
 
-      within('.search') do
-        expect(page).to have_field 'q', type: 'text'
-        expect(page).to have_button 'search'
-        fill_in('q', with: 'Roman')
-        click_on('search')
-        print "Clicked search\n"
-      end
-
-      within('#appliedParams') do
-          expect(page).to have_link 'startOverLink'
-      end
-
+    within('.search') do
+      expect(page).to have_field 'q', type: 'text'
+      expect(page).to have_button 'search'
+      fill_in('q', with: 'Roman')
+      click_on('search')
+      print "Clicked search\n"
     end
+
+    within('#appliedParams') do
+      expect(page).to have_link 'startOverLink'
+    end
+  end
 end
