9662424a42f09fae803c92b77398b6cc34ea26fc
Banurekha <banurekha.l@gmail.com>
2016-11-22 14:28:59 -0500

    cleaning up all rubocop offenses

--- spec/beehive/functional/beehive_test_spec.rb
+++ spec/beehive/functional/beehive_test_spec.rb
@@ -1,4 +1,5 @@
 
+# frozen_string_literal: true
 require 'rubygems'
 require 'capybara/rspec'
 require 'capybara/dsl'
@@ -11,20 +12,19 @@ Capybara.javascript_driver = :poltergeist
 Capybara.app_host = 'https://collections-pprd.library.nd.edu/'
 
 RSpec.configure do |config|
-    config.include Capybara::DSL
+  config.include Capybara::DSL
 end
 
-#Visiting Digital Exhibits and Collections - home page of Beehive.
+# Visiting Digital Exhibits and Collections - home page of Beehive.
 print "Testing"
 
-feature 'Load homepage', :js => true do
-    scenario 'lets user load Beehive homepage' do
-        visit '/'
-        puts current_url
+feature 'Load homepage', js: true do
+  scenario 'lets user load Beehive homepage' do
+    visit '/'
+    puts current_url
 
-        within('#content') do
-        expect(page).to have_text "Featured Collections"
-      end
+    within('#content') do
+      expect(page).to have_text "Featured Collections"
     end
+  end
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
 require 'rubygems'
-#use rspec/autorun only when you need to absolutely run *.rb files using ruby command
-#require 'rspec/autorun'
+# use rspec/autorun only when you need to absolutely run *.rb files using ruby command
+# require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
-#HighLine provides a robust system for requesting data from a user
+# HighLine provides a robust system for requesting data from a user
 require 'highline/import'
 require 'yaml'
 require 'capybara_error_intel/dsl'
 
 Capybara.run_server = false
 Capybara.current_driver = :poltergeist
-#Capybara.default_max_wait_time = 10 #This sets wait time globally
+# Capybara.default_max_wait_time = 10 #This sets wait time globally
 
-#If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
+# If you don't provide this, Capybara will pick  the selenium driver for javascript_driver by default
 Capybara.javascript_driver = :poltergeist
 
-#Gives access to the capybara methods
+# Gives access to the capybara methods
 RSpec.configure do |config|
   config.include Capybara::DSL
 
@@ -26,16 +27,16 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 
   config.before(:example) do |ex|
-    set_application_host_by_example(ex)
+    application_host_by_example(ex)
   end
 end
 
-def set_application_host_by_example(example)
+def application_host_by_example(example)
   spec_helper_path = File.dirname(__FILE__)
   spec_path = example.metadata.fetch(:absolute_file_path)
   spec_sub_directory = spec_path.sub("#{spec_helper_path}/", '').split('/').first
   # TODO: There may be a mix of environments that we want to test; How to handle this? The current assumption is test the same environment.
   environment = ENV.fetch('ENVIRONMENT', 'prod')
-  servers_by_environment = YAML::load_file(File.expand_path("./#{spec_sub_directory}/#{spec_sub_directory}_config.yml", spec_helper_path))
+  servers_by_environment = YAML.load_file(File.expand_path("./#{spec_sub_directory}/#{spec_sub_directory}_config.yml", spec_helper_path))
   Capybara.app_host = servers_by_environment.fetch(environment)
 end
