8d528974dff20a357965e09dae230d7db0d35614
Banurekha <banurekha.l@gmail.com>
2016-11-29 14:23:09 -0500

    adding spec for about page

--- spec/curate/pages/about_page.rb
+++ spec/curate/pages/about_page.rb
@@ -7,8 +7,47 @@ module Curate
       include CapybaraErrorIntel::DSL
 
       def on_page?
-        # Fill out selectors etc.
-        true
+        on_valid_url? &&
+          status_response_ok? &&
+          valid_page_content? &&
+          valid_page_navigation? &&
+          valid_links?
+      end
+
+      def on_valid_url?
+        current_url == File.join(Capybara.app_host, 'about')
+      end
+
+      def status_response_ok?
+        status_code == 200
+      end
+
+      def valid_page_content?
+        has_content?('ABOUT')
+      end
+
+      def valid_page_navigation?
+        within(".feature-navigation-wrapper") do
+          find_link('Manage').visible? &&
+            find_link('Preserve').visible? &&
+            find_link('Discover').visible? &&
+            find_link('Share').visible?
+        end
+      end
+
+      def valid_links?
+        valid_deposit_links? &&
+          valid_help_button?
+      end
+
+      def valid_deposit_links?
+        all(".emphatic-action-wrapper a").count.eql?(4)
+      end
+
+      def valid_help_button?
+        within(".page-footer-wrapper") do
+          find_link('Help').visible?
+        end
       end
     end
   end
