400e2c28eaaec9aa8ee06a312c676227cbd42c66
LaRita Robinson <lrobins5@nd.edu>
2016-11-30 14:01:29 -0500

    Make Rubocop happy

--- spec/curate/pages/category_search_page.rb
+++ spec/curate/pages/category_search_page.rb
@@ -12,17 +12,17 @@ module Curate
         thesis: "/catalog?f_inclusive%5Bhuman_readable_type_sim%5D%5B%5D=Doctoral+Dissertation&f_inclusive%5Bhuman_readable_type_sim%5D%5B%5D=Master%27s+Thesis",
         article: "/catalog?f%5Bhuman_readable_type_sim%5D%5B%5D=Article",
         dataset: "/catalog?f%5Bhuman_readable_type_sim%5D%5B%5D=Dataset"
-      }
+      }.freeze
       LOOKUP_CAPTION = {
         thesis: "Theses & Dissertations",
         article: "Articles & Publications",
         dataset: "Datasets & Related Materials"
-      }
+      }.freeze
       LOOKUP_VALUE = {
         thesis: "Doctoral Dissertation OR Master's Thesis",
-        article: "",
-        dataset: ""
-      }
+        article: "Article",
+        dataset: "Dataset"
+      }.freeze
 
       def initialize(category)
         @category = category
@@ -52,21 +52,21 @@ module Curate
         # Note: this should be found in the H2 row of main, but I couldn't get the right scope.
         # However, finding it on the page should be adequate.
         has_content?(category) &&
-        within('.sidebar') do
-          has_content?('Filter by:')
-        end
+          within('.sidebar') do
+            has_content?('Filter by:')
+          end
       end
 
       def valid_search_constraint?
         within('.search-constraint-notice') do
           has_content?('Search criteria:')
         end &&
-        within('.filter-name') do
-          has_content?('Type of Work:')
-        end
-        within('.filter-value') do
-          has_content?(LOOKUP_VALUE.fetch(category))
-        end
+          within('.filter-name') do
+            has_content?('Type of Work:')
+          end &&
+          within('.filter-value') do
+            has_content?(LOOKUP_VALUE.fetch(category))
+          end
       end
     end
   end
