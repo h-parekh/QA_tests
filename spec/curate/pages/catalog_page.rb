592ffbb3c29d5f5415bf6963b668b36a1d57fa65
LaRita Robinson <lrobins5@nd.edu>
2016-11-30 17:09:57 -0500

    Refactor catalog search page
    Combine category search page into catalog search page.

--- spec/curate/pages/catalog_page.rb
+++ spec/curate/pages/catalog_page.rb
@@ -6,8 +6,27 @@ module Curate
       include Capybara::DSL
       include CapybaraErrorIntel::DSL
 
-      def initialize(search_term = '')
-        @search_term = search_term
+      attr_reader :search_term, :category
+
+      LOOKUP_CATEGORY_URL = {
+        thesis: "/catalog?f_inclusive%5Bhuman_readable_type_sim%5D%5B%5D=Doctoral+Dissertation&f_inclusive%5Bhuman_readable_type_sim%5D%5B%5D=Master%27s+Thesis",
+        article: "/catalog?f%5Bhuman_readable_type_sim%5D%5B%5D=Article",
+        dataset: "/catalog?f%5Bhuman_readable_type_sim%5D%5B%5D=Dataset"
+      }.freeze
+      LOOKUP_CATEGORY_CAPTION = {
+        thesis: "Theses & Dissertations",
+        article: "Articles & Publications",
+        dataset: "Datasets & Related Materials"
+      }.freeze
+      LOOKUP_CATEGORY_VALUE = {
+        thesis: "Doctoral Dissertation OR Master's Thesis",
+        article: "Article",
+        dataset: "Dataset"
+      }.freeze
+
+      def initialize(options)
+        @search_term = options[:search_term]
+        @category = options[:category]
       end
 
       def on_page?
@@ -18,8 +37,14 @@ module Curate
       end
 
       def on_valid_url?
-        current_url.start_with?(File.join(Capybara.app_host, 'catalog')) &&
-          current_url.include?(@search_term)
+        if category.nil? && search_term.nil?
+          current_url.start_with?(File.join(Capybara.app_host, 'catalog'))
+        elsif category.nil?
+          current_url.start_with?(File.join(Capybara.app_host, 'catalog')) &&
+            current_url.include?(search_term)
+        else
+          current_url == File.join(Capybara.app_host, LOOKUP_CATEGORY_URL.fetch(category))
+        end
       end
 
       def on_base_url?
@@ -33,17 +58,37 @@ module Curate
       def valid_page_content?
         within('.sidebar') do
           has_content?('Filter by:')
-        end
+        end &&
+          # Note: this should be found in the H2 row of main, but I couldn't get the right scope test.
+          # However, simply finding it on the page should be adequate.
+          if category.nil?
+            true
+          else
+            has_content?(LOOKUP_CATEGORY_CAPTION.fetch(category))
+          end
       end
 
       def valid_search_constraint?
-        return true if @search_term.empty?
+        return true if search_term.nil? && category.nil?
+
         within('.search-constraint-notice') do
           has_content?('Search criteria:')
         end &&
           within('.filter-value') do
-            has_content?(@search_term)
+            has_content?(filter_value)
+          end &&
+          if category.nil?
+            true
+          else
+            within('.filter-name') do
+              has_content?('Type of Work:')
+            end
           end
+      end
+
+      def filter_value
+        return LOOKUP_CATEGORY_VALUE.fetch(category) unless category.nil?
+        search_term
       end
     end
   end
