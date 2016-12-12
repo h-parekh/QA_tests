c0a34d3ee813394087eacabd76a4a73bcc7c6e83
Banurekha <banurekha.l@gmail.com>
2016-12-12 10:42:45 -0500

    resolving code conflicts and fixing rubocop violations

--- spec/curate/functional/func_curate_spec.rb
+++ spec/curate/functional/func_curate_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 require 'curate/curate_spec_helper'
 
-feature 'User Browsing:', js: true do
-  scenario 'Load Homepage' do
+feature 'User Browsing', js: true do
+  scenario 'Test start: Load Homepage' do
     visit '/'
     home_page = Curate::Pages::HomePage.new
     expect(home_page).to be_on_page
@@ -100,15 +100,67 @@ feature 'Requesting Help', js: true do
     click_on('Submit')
     expect(page).to have_selector('#ajax-modal', visible: true)
   end
+end
 
-  scenario 'Test: Facet Navigation' do
+feature 'Facet Navigation', js: true do
+  scenario 'Department or Unit' do
     visit '/'
     click_on('Search')
     print "Testing #{current_url}\n"
-    expect(find('#ajax-modal').visible?).to be_falsey
+    expect(page).not_to have_selector("#ajax-modal")
     click_on('Department or Unit')
-    #expect(page).to have_selector('#ajax-modal', visible: false)
-    find(:css, "#ajax-modal").should be_visible
+    expect(page).to have_selector('#ajax-modal', visible: true)
+    expect(page).to have_content('Department or Unit')
     print "Clicked  Department or Unit \n"
+    within('#ajax-modal') do
+      find('.close').click
+    end
+    expect(page).not_to have_selector("#ajax-modal")
+  end
+
+  scenario 'Collection' do
+    visit '/'
+    click_on('Search')
+    print "Testing #{current_url}\n"
+    expect(page).not_to have_selector("#ajax-modal")
+    click_on('Collection')
+    expect(page).to have_selector('#ajax-modal', visible: true)
+    expect(page).to have_content('Collection')
+    print "Clicked  Department or Unit \n"
+    within('#ajax-modal') do
+      find('.close').click
+    end
+    expect(page).not_to have_selector("#ajax-modal")
+  end
+
+  scenario 'Other facets' do
+    visit '/'
+    click_on('Search')
+    facet_listing = ['Type_of_Work', 'Creator', 'Subject', 'Language', 'Publisher', 'Related_Resource(s)', 'Academic_Status'].freeze
+    within('ul.facets') do
+      facet_listing.each do |facet_name|
+        print "Testing #{facet_name}\n"
+        next unless has_content?(facet_name)
+        expect(page).not_to have_css("#collapse_#{facet_name}.in.collapse")
+        find("a[data-target=\"#collapse_#{facet_name}\"]").click
+        expect(page).to have_css("#collapse_#{facet_name}.in.collapse")
+      end
+    end
+  end
+
+  scenario 'Publisher' do
+    visit '/'
+    click_on('Search')
+    expect(page).not_to have_css('#collapse_Publisher.in.collapse')
+    find("a[data-target='#collapse_Publisher']").click
+    expect(page).to have_css('#collapse_Publisher.in.collapse')
+  end
+
+  scenario 'Academic Status' do
+    visit '/'
+    click_on('Search')
+    expect(page).not_to have_css('#collapse_Academic_Status.in.collapse')
+    find("a[data-target='#collapse_Academic_Status']").click
+    expect(page).to have_css('#collapse_Academic_Status.in.collapse')
   end
 end
