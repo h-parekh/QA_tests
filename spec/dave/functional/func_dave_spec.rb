7216da4e180343852ad8397e151c7e312b22326b
hparekh <hparekh@nd.edu>
2017-01-09 11:48:44 -0500

    Adding a sample test

--- spec/dave/functional/func_dave_spec.rb
+++ spec/dave/functional/func_dave_spec.rb
@@ -1,7 +1,12 @@
 require 'dave/dave_spec_helper'
 
+Capybara.current_driver = :selenium
+Capybara.javascript_driver = :selenium
+
 feature 'DAVE artifact viewing', js: true do
-  scenario 'Load 1st example' do
-    visit '/'
+  scenario 'Load 1st example', js: true do
+    visit '/0/MSN-COL_9101-1-B/0/1/0'
+    expect(page).to have_content('Essex County')
+    expect(page.current_url).to eq ('http://testlibnd-dave.s3-website-us-east-1.amazonaws.com/0/MSN-COL_9101-1-B/0/1/0')
   end
 end
