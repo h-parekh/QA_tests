36989b116c065ecf0e60b1522d718f357b89ddc5
hparekh <hparekh@nd.edu>
2017-01-09 12:20:38 -0500

    Removing 'js: true' from scenario

--- spec/dave/functional/func_dave_spec.rb
+++ spec/dave/functional/func_dave_spec.rb
@@ -4,7 +4,7 @@ Capybara.current_driver = :webkit
 Capybara.javascript_driver = :webkit
 
 feature 'DAVE artifact viewing', js: true do
-  scenario 'Load 1st example', js: true do
+  scenario 'Load 1st example' do
     visit '/0/MSN-COL_9101-1-B/0/1/0'
     expect(page).to have_content('Essex County')
     expect(page.current_url).to eq ('http://testlibnd-dave.s3-website-us-east-1.amazonaws.com/0/MSN-COL_9101-1-B/0/1/0')
