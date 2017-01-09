ff24b1939bee89c0aa00f0fd1d62da914fe93e52
hparekh <hparekh@nd.edu>
2017-01-09 16:19:54 -0500

    Removing deprecated warnings
    set :scm, :git has been deprecated. Hence adding these lines in Capfile
    require 'capistrano/scm/git'
    install_plugin Capistrano::SCM::Git

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ lock '3.7.1'
 set :application, 'QA_tests'
 set :repo_url, 'git@github.com:ndlib/QA_tests.git'
 set :deploy_to, '/home/app/QA_tests'
-set :scm, :git
 set :keep_releases, 5
 set :deploy_via, :copy
 set :stages, ["staging", "production"]
