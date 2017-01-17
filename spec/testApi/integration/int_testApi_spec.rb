fd58b9f5da10b9768e44c27af87dd559d9c74bba
hparekh <hparekh@nd.edu>
2017-01-17 10:05:42 -0500

    adding a test scenario using airborne

--- spec/testApi/integration/int_testApi_spec.rb
+++ spec/testApi/integration/int_testApi_spec.rb
@@ -1,1 +1,9 @@
 require 'testApi/testApi_spec_helper'
+
+describe 'Validate response' do
+  it 'should validate types' do
+    get 'http://testcontroller02.library.nd.edu:5602/api/v1/inventory'
+    expect_json_types(message: :string)
+    expect_status('200 OK')
+  end
+end
