3be640f2c5e4a0cf9dca69e72fa003cdc080e1de
hparekh <hparekh@nd.edu>
2017-01-17 10:11:11 -0500

    Reading local swagger def

--- spec/testApi/integration/int_testApi_spec.rb
+++ spec/testApi/integration/int_testApi_spec.rb
@@ -1,8 +1,11 @@
 require 'testApi/testApi_spec_helper'
 
+api = Swagger.load('/Users/hparekh/git/test_api/definitions/swagger.yaml')
+
 describe 'Validate response' do
   it 'should validate types' do
     get 'http://testcontroller02.library.nd.edu:5602/api/v1/inventory'
+    puts response
     expect_json_types(message: :string)
     expect_status('200 OK')
   end
