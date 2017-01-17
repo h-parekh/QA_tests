eb75b98033998a3863cb1a43e24c305de98ceaab
hparekh <hparekh@nd.edu>
2017-01-17 11:40:02 -0500

    WIP tests using airborne and swagger gem
    I'm using swagger-rb gem to read a swagger definition and the airborne
    gem to loop through the operations in the definition
    
    https://github.com/swagger-rb/swagger-rb
    https://github.com/brooklynDev/airborne
    
    How do I use the variables from api.operations object to call the
    endpoint get "/inventory"
    Something like exec(operation.verb operation.path)

--- spec/testApi/integration/int_testApi_spec.rb
+++ spec/testApi/integration/int_testApi_spec.rb
@@ -1,12 +1,20 @@
 require 'testApi/testApi_spec_helper'
+require 'airborne'
+require 'swagger'
 
 api = Swagger.load('/Users/hparekh/git/test_api/definitions/swagger.yaml')
+Airborne.configure do |config|
+  config.base_url = 'http://testcontroller02.library.nd.edu:5602' + api.basePath
+end
 
 describe 'Validate response' do
   it 'should validate types' do
-    get 'http://testcontroller02.library.nd.edu:5602/api/v1/inventory'
-    puts response
-    expect_json_types(message: :string)
-    expect_status('200 OK')
+  api.operations.each do | operation |
+    # controller_name = operation.path
+    # controller_action = operation.verb
+    puts operation.signature
+    # expect_json_types(message: :string)
+    # expect_status('200 OK')
+  end
   end
 end
