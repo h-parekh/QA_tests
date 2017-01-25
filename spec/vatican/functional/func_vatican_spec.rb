e6e6888303a6bb5cb570d89456b097414685655a
NicHaydel95 <nhaydel@nd.edu>
2017-01-25 13:24:09 -0500

    Load Home & Instructions tests done

--- spec/vatican/functional/func_vatican_spec.rb
+++ spec/vatican/functional/func_vatican_spec.rb
@@ -1,1 +1,17 @@
 require 'vatican/vatican_spec_helper'
+feature "User Browsing", js: true do
+  scenario 'Load Homepage' do
+    page.driver.browser.js_errors = false
+    visit '/'
+    home_page = Vatican::Pages::HomePage.new
+    expect(home_page).to be_on_page
+  end
+
+  scenario 'How To Use Database page' do
+    page.driver.browser.js_errors = false
+    visit '/'
+    click_on('How To Use the Database')
+    instructions_page = Vatican::Pages::InstructionsPage.new
+    expect(instructions_page).to be_on_page
+  end
+end
