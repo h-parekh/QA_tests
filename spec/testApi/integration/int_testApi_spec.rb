199bce72ede1e3a850bee0f8a048fef542f4f8f6
hparekh <hparekh@nd.edu>
2017-01-26 15:25:01 -0500

    removing case statements

--- spec/testApi/integration/int_testApi_spec.rb
+++ spec/testApi/integration/int_testApi_spec.rb
@@ -4,13 +4,7 @@ require 'testApi/testApi_spec_helper'
 describe 'API tests using swagger definition' do
   ApiConfig.swagger_object.operations.each do |operation|
     it "calls #{operation.verb} #{operation.path}" do
-      result = RestClient::Response.new
-      case operation.verb
-      when :get
-        result = get operation.path
-      when :post
-        result = post operation.path
-      end
+      result = public_send(operation.verb, operation.path)
       expect(result.code.to_s).to eq(operation.responses.keys[0])
       expect_json_types(message: :string)
     end
