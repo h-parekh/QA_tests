38538ea1ed9fc3d13de36005b053ff14f8239227
NicHaydel95 <nhaydel@nd.edu>
2017-03-06 13:33:10 -0500

    Working on web page crawler
    Currently determines the response code for all links on the page
    does not do any logging yet. I felt like it was best to write my
    own because we do not need recursive functionality and it would be
    easier to write into the current testing framework.

---
+++ spec/crawler/crawl/web_crawler.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+require 'crawler/crawler_spec_helper'
+require 'Nokogiri'
+require "net/http"
+require "uri"
+
+File.foreach(ENV["HOME"]+"/crawl_sites.txt") do |url|
+  url.delete!("\n")
+  puts
+  puts "Checking #{url}"
+  puts
+  encoded_url = URI.encode(url)
+  doc = Nokogiri::HTML(open(encoded_url))
+  root = url
+  links = doc.css('a')
+  hrefs = links.map {|link| link.attribute('href').to_s}.uniq.sort.delete_if {|href| href.empty?}
+  for link in hrefs do
+    if link.start_with?('/')
+      link = root+link
+    end
+    if link.include? "#"|| link == " " || link =~ /mailto:(.*)/|| link.nil?
+
+    else
+      encoded_link = URI.encode(link)
+      uri = URI.parse(link)
+      begin
+        response = Net::HTTP.get_response(uri)
+        puts "#{link} #{response.code}"
+        if response.code == 404
+          puts "Bad link #{link} on #{url}"
+      rescue SystemCallError
+        puts "Connection failed on: #{link}"
+        next
+      end
+    end
+  end
+end
