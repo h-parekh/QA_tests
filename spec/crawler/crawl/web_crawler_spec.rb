cc60c4bcdcff4fc0a7d10ccff7722c79a3f94913
NicHaydel95 <nhaydel@nd.edu>
2017-03-20 09:36:43 -0400

    Clean up web_crawler_spec.rb
    Removed rescue block and empty if statement

--- spec/crawler/crawl/web_crawler_spec.rb
+++ spec/crawler/crawl/web_crawler_spec.rb
@@ -7,7 +7,7 @@ feature 'Link Checker' do
   scenario 'Check Links' do
     File.foreach(ENV["HOME"] + "/crawl_sites.txt") do |url|
       url.delete!("\n")
-      current_logger.info(context: "Starting to verify: #{url}")
+      current_logger.info(context: "Begin verifying: #{url}")
       encoded_url = URI.encode(url)
       doc = Nokogiri::HTML(open(encoded_url))
       root = url
@@ -17,22 +17,14 @@ feature 'Link Checker' do
         if link.start_with?('/')
           link = root + link
         end
-        if link.include?("#") || link == " " || link =~ /mailto:(.*)/ || link.nil?
-        else
-          uri = URI.parse(link)
-          begin
-            response = Net::HTTP.get_response(uri)
-            if response.code.to_i >= 400 && response.code.to_i <= 599
-              current_logger.info(context: "crawler", url: link, status_code: response.code)
-            end
-          # Captures known errors from links to mailto: and tel:
-          # ex. mailto:email@nd.edu
-          rescue SystemCallError
-            current_logger.info(context: "Connection failed on: #{link}")
-            next
-          end
+        next if link.include?("#") || link == " " || link =~ /mailto:(.*)/ || link.nil? || link=~ /tel:(.*)/
+        uri = URI.parse(link)
+        response = Net::HTTP.get_response(uri)
+        if response.code.to_i >= 400 && response.code.to_i <= 599
+          current_logger.info(context: "crawler", url: link, status_code: response.code)
         end
       end
+      current_logger.info(context: "Finished verifying: #{url}")
     end
   end
 end
