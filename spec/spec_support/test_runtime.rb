acaf4553bd2125118c64e3b4c3fe319ccd80d333
hparekh <hparekh@nd.edu>
2017-04-17 12:21:02 -0400

    Making sure tmp/screenshots is created
    Tests will not run if temp/screenshots directory is not present.
    This change ensures its present and creates it if not.
    
    Also adding some documentation for clarity

--- spec/spec_support/test_runtime.rb
+++ spec/spec_support/test_runtime.rb
@@ -16,9 +16,15 @@ module RunIdentifier
     FileUtils.rm_rf(oldest_directory)
   end
 
+# * Create tmp/screenshots dir if not present
+# * Remove directories older than value specified in argument
+# * returns a relative path of the directory to which current test should save screenshots
+# @example 'tmp/screenshots/2017-04-17T12:03:57.859-05:00'
+# @return [String]
   def self.get_screenshots_save_path(runs: 5)
     screenshots_root = 'tmp/screenshots/'
     current_working_directory = Dir.pwd
+    FileUtils.mkdir_p(screenshots_root)
     Dir.chdir(screenshots_root)
     screenshots_directories = Dir.glob('*').select {|f| File.directory? f} # Returns a list of all screenshots directories in screenshots_root
     screenshots_directories.each_with_index{|dir, index|
@@ -26,7 +32,7 @@ module RunIdentifier
         FileUtils.rm_rf(dir)
       end
     }
-
+    # Using value of @run_identifier as directory name
     FileUtils.mkdir self.get
     Dir.chdir(current_working_directory) # Return to current directory so screenshots are in the right place
     File.join(screenshots_root,self.get)
