471645a5caf745e689a85feab47296f4e63f8534
Max Hallwachs <mhallwac@nd.edu>
2017-05-17 14:11:12 -0400

    Test Case for Use Case 1 - Microfilms

--- spec/microfilms/functional/func_microfilms_spec.rb
+++ spec/microfilms/functional/func_microfilms_spec.rb
@@ -2,20 +2,30 @@
 require 'microfilms/microfilms_spec_helper'
 
 feature 'User Browsing', js: true do
-  scenario 'Test 1: Enter text in search box' do
+  scenario 'Test 1: Loads Home Page' do
     visit '/'
     puts current_url
+
+    expect(page).to have_content 'About Searching'
 
     within('.search') do
       expect(page).to have_field 'q', type: 'text'
       expect(page).to have_button 'search'
-      fill_in('q', with: 'Roman')
-      click_on('search')
-      print "Clicked search\n"
     end
 
-    within('#appliedParams') do
-      expect(page).to have_link 'startOverLink'
+    within('.facets') do
+      expect(page).to have_link 'Format'
+      expect(page).to have_link 'Library'
+      expect(page).to have_link 'City'
+      expect(page).to have_link 'Country of Origin'
+      expect(page).to have_link 'Collection'
+      expect(page).to have_link 'Language'
+      expect(page).to have_link 'Date Range'
+      expect(page).to have_link 'Illuminations'
+      expect(page).to have_link 'Musical Notation'
+      expect(page).to have_link 'Commentary Volume'
+      expect(page).to have_link 'Type of Text'
     end
+
   end
 end
