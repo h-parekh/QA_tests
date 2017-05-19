6d9b3903efd678ef5b008c58606266d974f65f96
Max Hallwachs <mhallwac@nd.edu>
2017-05-19 12:41:37 -0400

    Creates scenario 'Test 2: Visit Story Index' Makes sure that all links in the header are on the page. Makes sure that the Keep in Touch tab is on the page Tests to see if all 10 chapter headers and pictures are on the page and are links. Tests to see if all of the specific stories are on the pages as links.

--- spec/hesburghsite/functional/func_hesburghsite_spec.rb
+++ spec/hesburghsite/functional/func_hesburghsite_spec.rb
@@ -18,4 +18,26 @@ feature 'User Browsing', js: true do
     expect(page).to have_selector('.flipster.flipster-active.flipster-carousel')
     expect(page).to have_selector('#timeline')
   end
+
+  scenario 'Test 2: Visit Story Index' do
+    page.driver.browser.js_errors = false
+    visit '/'
+    click_on('Story Index')
+    within('.signup') do
+      expect(page).to have_link('Keep in Touch')
+    end
+    within('.top-bar') do
+      expect(page).to have_link('Home')
+      expect(page).to have_link('Story Index')
+      expect(page).to have_link('Media Gallery')
+      expect(page).to have_link('Speeches')
+      expect(page).to have_link('Further Research')
+      expect(page).to have_link('About')
+    end
+    within('.large-12.columns.contents') do
+      expect(page).to have_css('h2 a', count: 10)
+      expect(page).to have_css('a img', count: 10)
+      expect(page).to have_css('li a', count: 57)
+    end
+  end
 end
