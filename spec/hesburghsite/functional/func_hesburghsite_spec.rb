a8c5bf842d8fc3bfb58a4cb0eb486878aa8f4121
Max Hallwachs <mhallwac@nd.edu>
2017-05-19 14:26:08 -0400

    Creates scenario 'Test 3' Tests whether the Home, Story Index, etc. links are on the page. Tests whether the Keep in Touch tab is on the page. Checks if the photo and the introduction text are on the page. Checks if the 'Featured Stories' links are on the page under the introduction text. Makes sure the document and photo gallery buttons are on page/links.

--- spec/hesburghsite/functional/func_hesburghsite_spec.rb
+++ spec/hesburghsite/functional/func_hesburghsite_spec.rb
@@ -40,4 +40,35 @@ feature 'User Browsing', js: true do
       expect(page).to have_css('li a', count: 57)
     end
   end
+
+  scenario 'Test 3: Click on chapter on Story Index page' do
+    page.driver.browser.js_errors = false
+    visit '/'
+    click_on('Story Index')
+    click_on('The Early Years')
+    within('.signup') do
+      expect(page).to have_link('Keep in Touch')
+    end
+    within('.top-bar') do
+      expect(page).to have_link('Home')
+      expect(page).to have_link('Story Index')
+      expect(page).to have_link('Media Gallery')
+      expect(page).to have_link('Speeches')
+      expect(page).to have_link('Further Research')
+      expect(page).to have_link('About')
+    end
+    within('.large-12.columns') do
+      expect(page).to have_css('img')
+      expect(page).to have_css('p')
+    end
+    within('.large-6.columns.featured') do
+      expect(page).to have_css('h1')
+      expect(page).to have_css('h2 a')
+    end
+    within('.large-4.large-offset-2.columns') do
+        ['Photo Gallery', 'Document Gallery'].shuffle.each do |name|
+            expect(page).to have_link(name)
+        end
+    end
+  end
 end
