39b9ffad4adb60d44b8b5b107c638f5998e048c9
hparekh <hparekh@nd.edu>
2017-05-24 13:59:33 -0400

    Updating hours spec to use ResponseValidator class

--- spec/hours/integration/int_hours_spec.rb
+++ spec/hours/integration/int_hours_spec.rb
@@ -1,12 +1,12 @@
 #frozen_string_literal: true
 require 'hours/hours_spec_helper'
 
-describe 'Hours API test' do
+feature 'Hours API test' do
   SwaggerHandler.operations(for_file_path: __FILE__).each do |operation|
-    it "calls #{operation.verb} #{operation.path}" do
+    scenario "calls #{operation.verb} #{operation.path}" do
       result = public_send(operation.verb, operation.url)
-      expect(result.code.to_s).to eq(operation.responses.keys[0])
-      #expect_json_types(redirect: :string)
+      current_response = ResponseValidator.new(operation, result)
+      expect(current_response).to be_valid_response
     end
   end
 end
