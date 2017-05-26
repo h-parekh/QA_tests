80d0068ea7d251e8c1c213871876ff016478cdf0
Max Hallwachs <mhallwac@nd.edu>
2017-05-26 11:32:03 -0400

    Updates the int_gatekeeper_spec.rb Removes previous ["authorization"] line because it does not work. Instead, the test uses the RequestBuilder class to create a schema variable with the current operation as it parameter.  Then uses the send_security_vs_none method to store the public_send result in the result variable which is used to validate the response.

--- spec/gatekeeper/integration/int_gatekeeper_spec.rb
+++ spec/gatekeeper/integration/int_gatekeeper_spec.rb
@@ -1,11 +1,11 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 require 'gatekeeper/gatekeeper_spec_helper'
 
 feature 'Gatekeeper API test' do
   SwaggerHandler.operations(for_file_path: __FILE__).each do |operation|
     scenario "calls #{operation.verb} #{operation.path}" do
-      request['authorization'] = "Token token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Im5ldGlkIjoidF9oZXNsaWIwMSJ9fQ.6MDSyPVHwx7ZFtsY6fQww4rj6shLHiqtmMXOCLrD89s"
-      result = public_send(operation.verb, operation.url)
+      schema = RequestBuilder.new(operation)
+      result = schema.send_security_vs_none
       current_response = ResponseValidator.new(operation, result)
       expect(current_response).to be_valid_response
     end
