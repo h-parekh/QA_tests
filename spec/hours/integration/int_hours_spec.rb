220fdfc5e2e674363141df52e5dbf2862be69b46
Max Hallwachs <mhallwac@nd.edu>
2017-06-02 10:41:21 -0400

    Updates RequestBuilder to avoid 'options' verb Creates if statement that avoids sending a public_send request if the operation verb is 'options' because we do not want to send that request. Also updates the hours integration test to use the RequestBuilder.

--- spec/hours/integration/int_hours_spec.rb
+++ spec/hours/integration/int_hours_spec.rb
@@ -4,7 +4,8 @@ require 'hours/hours_spec_helper'
 feature 'Hours API test' do
   SwaggerHandler.operations(for_file_path: __FILE__).each do |operation|
     scenario "calls #{operation.verb} #{operation.path}" do
-      result = public_send(operation.verb, operation.url)
+      schema = RequestBuilder.new(operation)
+      result = schema.send_security_vs_none
       current_response = ResponseValidator.new(operation, result)
       expect(current_response).to be_valid_response
     end
