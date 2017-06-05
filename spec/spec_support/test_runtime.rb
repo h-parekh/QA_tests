9924cdfefcb42c542136e49bc0c2895b10878155
hparekh <hparekh@nd.edu>
2017-06-05 11:21:09 -0400

    Adding documentation

--- spec/spec_support/test_runtime.rb
+++ spec/spec_support/test_runtime.rb
@@ -2,6 +2,8 @@
 require 'fileutils'
 
 module RunIdentifier
+  # * Provides getter and setter methods to
+  # create a unique ID for identifying the test run
   def self.set
     @run_identifier = DateTime.now.strftime("%Y-%m-%dT%H:%M:%S.%L-05:00")
   end
