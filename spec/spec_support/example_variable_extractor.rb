6f564b3c9f38007195fbf13425ac21d4ece39e66
hparekh <hparekh@nd.edu>
2017-06-06 10:34:00 -0400

    Making ExampleVariableExtractor return more fields
    I'm adding path_to_spec_directory and environment_under_test in
    the object returned by the struct.
    These are essential for the commit following this one.

--- spec/spec_support/example_variable_extractor.rb
+++ spec/spec_support/example_variable_extractor.rb
@@ -1,14 +1,15 @@
-ExampleVariable = Struct.new(:application_name_under_test, :test_type)
+ExampleVariable = Struct.new(:application_name_under_test, :test_type, :path_to_spec_directory, :environment_under_test)
 
 module ExampleVariableExtractor
   # @param path [String] The path to the spec file that is being tested
   # @return [ExampleStruct] An object that responds to #application_name_under_test and #test_type
   # @note If we start nesting our specs, this may need to be revisited.
-  def self.call(path:)
+  def self.call(path:, config:)
     path_to_spec_directory = File.expand_path('../../', __FILE__)
     spec_sub_directory = path.sub("#{path_to_spec_directory}/", '').split('/')
     application_name_under_test = spec_sub_directory[0]
     test_type = spec_sub_directory[1]
-    ExampleVariable.new(application_name_under_test, test_type)
+    environment_under_test = config.fetch('ENVIRONMENT', ExampleLogging::DEFAULT_ENVIRONMENT)
+    ExampleVariable.new(application_name_under_test, test_type, path_to_spec_directory, environment_under_test)
   end
 end
