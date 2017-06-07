1310816673eed6718d400c9bc47a166943f7d9f8
hparekh <hparekh@nd.edu>
2017-06-07 16:14:17 -0400

    Forcing PhantomJS to send request over TLSv1.2
    The OSF website (htttps://osf.io/ does not like requests coming
    over any other protocol other than TLSv1.2.
    Without this setting of poltergeist driver, by default all requests are
    sent over SSL and osf.io rejects that request at the network layer
    returning back with an error:
    ```console
    Capybara::Poltergeist::StatusFailError Exception:
    Request to 'https://osf.io/' failed to reach server,
    check DNS and/or server status
    ```

--- spec/osf/osf_spec_helper.rb
+++ spec/osf/osf_spec_helper.rb
@@ -3,3 +3,12 @@ require 'spec_helper'
 Dir.glob(File.expand_path('../pages/**/*.rb', __FILE__)).each do |filename|
   require filename
 end
+
+Capybara.register_driver :poltergeist do |app|
+  options = {
+       phantomjs_options: [
+         '--ssl-protocol=tlsv1.2'
+       ]
+     }
+  Capybara::Poltergeist::Driver.new(app, options)
+end
