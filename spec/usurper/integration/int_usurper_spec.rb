5267b0d4b3769ccb9835fa6d6fc7a7dd5e7df4ec
hparekh <hparekh@nd.edu>
2017-06-21 16:38:48 -0400

    Scenario WIP

--- spec/usurper/integration/int_usurper_spec.rb
+++ spec/usurper/integration/int_usurper_spec.rb
@@ -1,2 +1,11 @@
 # frozen_string_literal: true
-require 'viceroy/viceroy_spec_helper'
+require 'usurper/usurper_spec_helper'
+
+feature 'Test for Usurper content management API' do
+  scenario "Creates an entry" do
+    created_entry = ContentfulHandler.new(for_file_path: __FILE__, config: ENV, current_logger: current_logger)
+    expect(created_entry).to be_in_contentful
+    # Write a function to preview the entry just created using Content Preview API
+    # Remove the entry created using the Content Management API
+  end
+end
