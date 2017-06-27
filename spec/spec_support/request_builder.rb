e9e71ce7b88336cdb8e5a69527ae55d49f613ab7
hparekh <hparekh@nd.edu>
2017-06-27 16:18:53 -0400

    rubocop changes

--- spec/spec_support/request_builder.rb
+++ spec/spec_support/request_builder.rb
@@ -6,7 +6,7 @@ require 'csv'
 class RequestBuilder
   attr_reader :current_logger
 
-  def initialize(logger,operation)
+  def initialize(logger, operation)
     @current_operation = operation
     @security_name = get_security_name
     @token = get_token
@@ -34,7 +34,7 @@ class RequestBuilder
 
   def get_path_parameter!
     user_home_dir = File.expand_path('~')
-    parameter_list = CSV.read(user_home_dir+"/test_data/QA/aleph_system_ids.csv")
+    parameter_list = CSV.read(user_home_dir + "/test_data/QA/aleph_system_ids.csv")
     parameter_list.sample
   end
 
@@ -42,7 +42,7 @@ class RequestBuilder
     user_home_dir = File.expand_path('~')
     # Accesses the file to get the jwt token
     yaml = YAML.load_file(File.join("#{user_home_dir}", 'test_data/QA/jwt_token.yml'))
-    token = yaml.fetch('token')
+    yaml.fetch('token')
   end
 
   def get_security_name
@@ -52,14 +52,14 @@ class RequestBuilder
       # Get the type, name, and in fields of the security
       schema_security = schema.securityDefinitions.values[0]
       # Gets the name of the security to use for authorization
-      name = schema_security.name
+      schema_security.name
     end
   end
 
   def send_via_operation_verb(*body)
     # determines which method verb the current opertation has and sends request accordingly
     security = check_security
-    case (@current_operation.verb.to_s)
+    case @current_operation.verb.to_s
     when "get"
       current_logger.info(context: "making GET request")
       if security
@@ -74,21 +74,21 @@ class RequestBuilder
       else
         RestClient.public_send(@current_operation.verb, @current_operation.url, body)
       end
-    when "put" #might need  specific item when testing
+    when "put" # might need  specific item when testing
       current_logger.info(context: "naking PUT request")
       if security
         RestClient.public_send(@current_operation.verb, @current_operation.url, body, "#{@security_name}": "#{@token}")
       else
         RestClient.public_send(@current_operation.verb, @current_operation.url, body)
       end
-    when "patch" #might need specific item when testing
+    when "patch" # might need specific item when testing
       current_logger.info(context: "making PATCH request")
       if security
         RestClient.public_send(@current_operation.verb, @current_operation.url, body, "#{@security_name}": "#{@token}")
       else
         RestClient.public_send(@current_operation.verb, @current_operation.url, body)
       end
-    when "delete" #might need specific item when testing
+    when "delete" # might need specific item when testing
       current_logger.info(context: "making DELETE request")
       if security
         RestClient.public_send(@current_operation.verb, @current_operation.url, "#{@security_name}": "#{@token}")
