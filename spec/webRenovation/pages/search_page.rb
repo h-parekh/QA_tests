fd259764e3ccba286b72b27c439dce294b7e32ee
Max Hallwachs <mhallwac@nd.edu>
2017-06-29 14:36:46 -0400

    gets rid of the magic strings for urls and gives them a meaningful variable name

--- spec/webRenovation/pages/search_page.rb
+++ spec/webRenovation/pages/search_page.rb
@@ -4,6 +4,11 @@ module WebRenovation
       include Capybara::DSL
       include CapybaraErrorIntel::DSL
 
+      def initialize
+        @one_search_url = 'http://onesearch.library.nd.edu/primo_library/libweb/action/dlSearch.do?bulkSize=10&dym=true&highlight=true&indx=1&institution=NDU&mode=Basic&onCampus=false&pcAvailabiltyMode=true&query=any%2Ccontains%2C&search_scope=malc_blended&tab=onesearch&vid=NDU&displayField=title&displayField=creator'
+        @nd_catlog_search_url = 'http://onesearch.library.nd.edu/primo_library/libweb/action/dlSearch.do?bulkSize=10&dym=true&highlight=true&indx=1&institution=NDU&mode=Basic&onCampus=false&pcAvailabiltyMode=true&query=any%2Ccontains%2C&search_scope=nd_campus&tab=nd_campus&vid=NDU&displayField=title&displayField=creator'
+      end
+
       def on_page?
         valid_url? &&
         correct_content? &&
@@ -11,8 +16,8 @@ module WebRenovation
       end
 
       def valid_url?
-        current_url == 'http://onesearch.library.nd.edu/primo_library/libweb/action/dlSearch.do?bulkSize=10&dym=true&highlight=true&indx=1&institution=NDU&mode=Basic&onCampus=false&pcAvailabiltyMode=true&query=any%2Ccontains%2C&search_scope=malc_blended&tab=onesearch&vid=NDU&displayField=title&displayField=creator' ||
-        current_url == 'http://onesearch.library.nd.edu/primo_library/libweb/action/dlSearch.do?bulkSize=10&dym=true&highlight=true&indx=1&institution=NDU&mode=Basic&onCampus=false&pcAvailabiltyMode=true&query=any%2Ccontains%2C&search_scope=nd_campus&tab=nd_campus&vid=NDU&displayField=title&displayField=creator'
+        current_url == @one_search_url ||
+        current_url == @nd_catlog_search_url
       end
 
       def correct_content?
@@ -26,7 +31,7 @@ module WebRenovation
         page.has_selector?('#tab_dat_l.tab') &&
         page.has_selector?('.tab.EXLSearchTabTitle.EXLSearchTabLABELeBooks') &&
         page.has_selector?('#goButton') &&
-        if current_url == 'http://onesearch.library.nd.edu/primo_library/libweb/action/dlSearch.do?bulkSize=10&dym=true&highlight=true&indx=1&institution=NDU&mode=Basic&onCampus=false&pcAvailabiltyMode=true&query=any%2Ccontains%2C&search_scope=nd_campus&tab=nd_campus&vid=NDU&displayField=title&displayField=creator'
+        if current_url == @nd_catlog_search_url
           page.has_selector?('#showMoreOptions') &&
           page.has_selector?('.EXLSearchFieldMaximized.srch-box')
         else
