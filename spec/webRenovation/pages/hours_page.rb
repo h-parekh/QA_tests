304dae39179539fa33dfa7ef0d3b993fc79702c4
Max Hallwachs <mhallwac@nd.edu>
2017-06-30 12:39:48 -0400

    Creates an hours_page.rb file and scenario 'Go to Hours page' The hours page file checks to make sure all of the hours for all of the libraries are on the page.  The hours_page file also checks to make sure the url is correct for the page.  The scenario tests the functionality of going to the hours page from the homepage.

---
+++ spec/webRenovation/pages/hours_page.rb
@@ -0,0 +1,33 @@
+module WebRenovation
+  module Pages
+    class HoursPage < BasePage
+      include Capybara::DSL
+      include CapybaraErrorIntel::DSL
+
+      def on_page?
+        super &&
+        correct_content? &&
+        correct_url?
+      end
+
+      def correct_content?
+        page.has_selector?('.location', text: 'Hesburgh Library')
+        page.has_selector?('.location', text: 'Engineering Library')
+        page.has_selector?('.location', text: 'O\'Meara Mathematics Library')
+        # comment below is a placeholder for that code until the bug on the webite is fixed(Medieval shown twice)
+        # page.has_selector?('.location', text: 'Medieval Institute Library')
+        page.has_selector?('.location', text: 'Rare Books & Special Collections')
+        page.has_selector?('.location', text: 'Chemistry-Physics Library')
+        page.has_selector?('.location', text: 'Visual Resources Center')
+        page.has_selector?('.location', text: 'Architecture Library')
+        page.has_selector?('.location', text: 'Radiation Chemistry Reading Room')
+        page.has_selector?('.location', text: 'Mahaffey Business Library')
+        page.has_selector?('.location', text: 'Kellogg Kroc Library')
+      end
+
+      def correct_url?
+        current_url == (Capybara.app_host + "hours")
+      end
+    end
+  end
+end
