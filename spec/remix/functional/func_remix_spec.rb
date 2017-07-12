d3f0c909c635d86bd5fd2e869e5607ce80dbbadf
Max Hallwachs <mhallwac@nd.edu>
2017-07-12 13:51:51 -0400

    Creates functional test for login to Remix ND Creates the file for the functional login test for remix ND.
    Creates the config and spec_helper for the test.
    
    Creates the necessary folders

---
+++ spec/remix/functional/func_remix_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'remix/remix_spec_helper'
+
+feature 'Institutional Login', js: true do
+  let(:login_page) { LoginPage.new(current_logger, account_details_updated: false) }
+  scenario 'Sign in by institution (Notre Dame)' do
+    visit '/Shibboleth.sso/Login?target=https%3A%2F%2Fremix.nd.edu%2F%3Fq%3Dshib_login%2Fnode%2F27'
+    login_page.completeLogin
+    expect(page).to have_selector('.header__site-link')
+    expect(current_url).to eq('https://remix.nd.edu/?q=node/27')
+  end
+end
