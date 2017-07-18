72b5691fe8f545153fb4f04f37dbf99289c7342f
hparekh <hparekh@nd.edu>
2017-07-18 14:53:15 -0400

    Adjusting assertions for change in feedback design
    * Removing assertion for 'Submit feedback' button
    * Changing assertion for 'Feedback' to 'Website Feedback'
    * Removing href option from find('Website Feedback') to remove
    dependency on the URL that can change

--- spec/webRenovation/pages/base_page.rb
+++ spec/webRenovation/pages/base_page.rb
@@ -11,8 +11,7 @@ module WebRenovation
       def on_page?
         status_response_ok? &&
           valid_header? &&
-          valid_footer? &&
-          valid_feedback_button?
+          valid_footer?
       end
 
       def status_response_ok?
@@ -72,7 +71,7 @@ module WebRenovation
 
       def valid_footer?
         within('#footer-links') do
-          find_link('Feedback', href: 'https://docs.google.com/a/nd.edu/forms/d/e/1FAIpQLSdL4MnInHvXcQke9dJQ1Idkv2O23u9dBV_9ky40WDOV77B_MA/viewform?c=0&w=1').visible? &&
+          find_link('Website Feedback').visible? &&
             find_link('Library Giving', href: 'http://librarygiving.nd.edu').visible? &&
             find_link('Jobs', href: '/employment/').visible? &&
             find_link('Hesnet', href: 'https://wiki.nd.edu/display/libintranet/Home').visible? &&
@@ -82,12 +81,6 @@ module WebRenovation
 
         within('#chat') do
           find('a.chat-button').visible?
-        end
-      end
-
-      def valid_feedback_button?
-        within('.feedback-notice-me') do
-          find_link('SUBMIT FEEDBACK', href: 'https://docs.google.com/a/nd.edu/forms/d/e/1FAIpQLSdL4MnInHvXcQke9dJQ1Idkv2O23u9dBV_9ky40WDOV77B_MA/viewform?c=0&w=1').visible?
         end
       end
     end
