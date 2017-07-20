687a6e25df16893949e61e274e020645ba1c17e8
hparekh <hparekh@nd.edu>
2017-07-20 09:16:54 -0400

    Suppressing js errors in old library site

--- spec/oldlibrarysite/functional/func_oldlibrarysite_spec.rb
+++ spec/oldlibrarysite/functional/func_oldlibrarysite_spec.rb
@@ -4,6 +4,7 @@ require 'oldlibrarysite/oldlibrarysite_spec_helper'
 feature 'Institutional Login', js: true do
   let(:login_page) { LoginPage.new(current_logger, account_details_updated: false) }
   scenario 'Sign in by institution (Notre Dame)' do
+    page.driver.browser.js_errors = false # JS erors on some of the pages
     visit '/'
     click_on('Log In')
     login_page.completeLogin
