0c66064b113bb023ae2bf072f247f57a0da6f44d
hparekh <hparekh@nd.edu>
2017-09-22 15:18:44 -0400

    Adding code to require release number as input

--- spec/contentful/contentful_spec_helper.rb
+++ spec/contentful/contentful_spec_helper.rb
@@ -6,3 +6,9 @@ require 'contentful'
 Dir.glob(File.expand_path('../pages/**/*.rb', __FILE__)).each do |filename|
   require filename
 end
+
+RSpec.configure do |config|
+  config.before(:example) do
+    InitializeExample.require_release_number
+  end
+end
