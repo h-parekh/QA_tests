f5435fe1a175ad8edf0fd3c94c7c8c1892ab1512
hparekh <hparekh@nd.edu>
2017-09-22 10:47:16 -0400

    Updating comments

--- spec/contentful/integration/int_contentful_spec.rb
+++ spec/contentful/integration/int_contentful_spec.rb
@@ -5,13 +5,13 @@ feature 'Test for Usurper content management API' do
   scenario "Creates, previews, and deletes an entry" do
     ContentfulHandler.create(current_logger: current_logger) do |entry|
       expect(entry).not_to be_published
-      # Write a function to preview the entry just created using Content Preview API
+      # Preview the entry just created using Content Preview API
       entry.make_previewable!
       visit "/#{entry.slug}?preview=true"
-      # Remove the entry created using the Content Management API
       test_page_preview = ContentfulTests::Pages::TestPagePreview.new(contentful_entry: entry)
       expect(test_page_preview).to be_on_page
       expect(entry).not_to be_deleted
+      # Remove the entry created using the Content Management API
       entry.delete
       expect(entry).to be_deleted
     end
