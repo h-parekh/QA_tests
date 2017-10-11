b4d74000c049052e9a4d005f4eb2238292146fd3
hparekh <hparekh@nd.edu>
2017-10-11 15:39:57 -0400

    Adding assertions for query params in redirect url
    * Since we're not testing the full use case of redirecting back
    after ORCID sign in, these assertions provide a certain degree of
    validation that the site fires a correct redirect URL
    * Also updating the on_valid_url? method to assert on just the
    hostname instead of the entire URL.

--- spec/curate/pages/orcid_home_page.rb
+++ spec/curate/pages/orcid_home_page.rb
@@ -8,11 +8,12 @@ module Curate
       def on_page?
         on_valid_url? &&
           status_response_ok? &&
-          valid_page_content?
+          valid_page_content? &&
+          valid_uri_parameters?
       end
 
       def on_valid_url?
-        current_url.include?('orcid.org')
+        Capybara.current_host.include?('orcid.org')
       end
 
       def status_response_ok?
@@ -23,6 +24,16 @@ module Curate
         page.has_content?("ORCID")
         page.has_selector?(:link_or_button, "Register now")
         page.has_selector?(:link_or_button, "Sign into ORCID")
+      end
+
+      def valid_uri_parameters?
+        # Since we're not testing the full use case of redirecting back after ORCID sign in,
+        # these assertions provide a certain degree of validation that the site fires a correct redirect URL
+        hostname_to_redirect = URI.parse(Capybara.app_host).host
+        redirect_url_to_check = 'redirect_uri=https%3A%2F%2F' + hostname_to_redirect + '%2Forcid%2Fcreate_orcid'
+        current_url.include?(redirect_url_to_check)
+        current_url.include?('response_type=code')
+        current_url.include?('scope=%2Fread-limited+%2Factivities%2Fupdate')
       end
     end
   end
