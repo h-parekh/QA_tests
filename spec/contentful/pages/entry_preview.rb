a468248c15c89bb51e02cfd7166a759bf6555584
hparekh <hparekh@nd.edu>
2017-10-27 14:20:30 -0400

    Adding a event preview page

---
+++ spec/contentful/pages/entry_preview.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+module ContentfulTests
+  module Pages
+    class EntryPreview
+      include Capybara::DSL
+      include CapybaraErrorIntel::DSL
+
+      def initialize(contentful_entry)
+        @contentful_entry = contentful_entry.fetch(:contentful_entry)
+      end
+
+      def on_page?
+        correct_url? &&
+          correct_content?
+      end
+
+      def correct_content?
+        find('.page-title', text: "#{@contentful_entry.title}")
+      end
+
+      def correct_url?
+        current_url == File.join(Capybara.app_host, "/event/#{@contentful_entry.slug}?preview=true")
+      end
+    end
+  end
+end
