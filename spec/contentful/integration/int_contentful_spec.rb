85eefde818dfe3c9d2bccadb3609cfb8a74ee076
hparekh <hparekh@nd.edu>
2017-10-27 15:43:14 -0400

    Adding comments and TODO items for better clarity and tracking

--- spec/contentful/integration/int_contentful_spec.rb
+++ spec/contentful/integration/int_contentful_spec.rb
@@ -22,10 +22,18 @@ feature 'Tests for Contentful entries and webhook integrations' do
   # https://github.com/ndlib/monarch_libguides/blob/master/deploy/gateway.yml
   # I am calling /newevent via contentful to make sure the webhooks are firing
   # correctly, and the event is then previewable
+  # 'lib_cal_id' is a unique ID that is used to pull data from the libcal system
+  # TODO: Parameterize libcal ID, we should leverage the PathParameterizer::ParameterFinder
+  # class. It probably needs some refactor becuase it's designed to parameterize
+  # endpoints for path and query parameters
+  # Tracking here: https://github.com/ndlib/QA_tests/issues/233
   scenario "Create and preview an entry of type 'Events'" do
     ContentfulHandler.create(current_logger: current_logger, content_type: 'event', lib_cal_id: '3596276') do |entry|
       entry.verify_webhooks
       expect(entry).not_to be_published
+      # This isn't elegant but necessary in this case, because we want to wait for the
+      # new event to reach out to Libcal via the monarchLibguides /newevent endpoint
+      # and update event information.
       sleep(10)
       # At this point the event entry should have been updated in contentful, so we need to refresh it
       entry.refresh_entry
