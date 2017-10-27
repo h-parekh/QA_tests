12b461b0708ad5ecbd69f53f341a44de43cd7b40
hparekh <hparekh@nd.edu>
2017-10-27 14:21:44 -0400

    Adding contentful scenario for libguides events

--- spec/contentful/integration/int_contentful_spec.rb
+++ spec/contentful/integration/int_contentful_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 require 'contentful/contentful_spec_helper'
 
-feature 'Test for Usurper content management API' do
-  scenario "Checks webhooks; creates, previews, and deletes an entry" do
+feature 'Tests for Contentful entries and webhook integrations' do
+  scenario "Create and preview an entry of type 'Page'" do
     ContentfulHandler.create(current_logger: current_logger) do |entry|
       entry.verify_webhooks
       expect(entry).not_to be_published
@@ -11,6 +11,28 @@ feature 'Test for Usurper content management API' do
       visit "/#{entry.slug}?preview=true"
       test_page_preview = ContentfulTests::Pages::TestPagePreview.new(contentful_entry: entry)
       expect(test_page_preview).to be_on_page
+      expect(entry).not_to be_deleted
+      # Remove the entry created using the Content Management API
+      entry.delete
+      expect(entry).to be_deleted
+    end
+  end
+  
+  # This scenario is testing the /newevent endpoint of monarch_libguides API
+  # https://github.com/ndlib/monarch_libguides/blob/master/deploy/gateway.yml
+  # I am calling /newevent via contentful to make sure the webhooks are firing
+  # correctly, and the event is then previewable
+  scenario "Create and preview an entry of type 'Events'" do
+    ContentfulHandler.create(current_logger: current_logger, content_type: 'event', lib_cal_id: '3596276') do |entry|
+      entry.verify_webhooks
+      expect(entry).not_to be_published
+      sleep(10)
+      # At this point the event entry should have been updated in contentful, so we need to refresh it
+      entry.refresh_entry
+      entry.make_previewable!
+      visit "/event/#{entry.slug}?preview=true"
+      entry_preview = ContentfulTests::Pages::EntryPreview.new(contentful_entry: entry)
+      expect(entry_preview).to be_on_page
       expect(entry).not_to be_deleted
       # Remove the entry created using the Content Management API
       entry.delete
--- spec/monarchLibguides/integration/int_monarchLibguides_spec.rb
+++ spec/monarchLibguides/integration/int_monarchLibguides_spec.rb
@@ -11,26 +11,3 @@ feature 'monarchLibguides API tests' do
     end
   end
 end
-
-# This scenario is testing the /newevent endpoint of monarch_libguides API
-# https://github.com/ndlib/monarch_libguides/blob/master/deploy/gateway.yml
-# I am calling /newevent via contentful to make sure the webhooks are firing
-# correctly, and the event is then previewable
-feature 'Test for libguides event updater' do
-  scenario "Checks webhooks; creates an event, saves it, wait 30 sec, preview and deletes libduige entry" do
-    ContentfulHandler.create(current_logger: current_logger, content_type: 'event', lib_cal_id: '3596276') do |entry|
-      require 'byebug'; debugger
-      entry.verify_webhooks
-      expect(entry).not_to be_published
-      # Preview the entry just created using Content Preview API
-      entry.make_previewable!
-      visit "/#{entry.slug}?preview=true"
-      test_page_preview = ContentfulTests::Pages::TestPagePreview.new(contentful_entry: entry)
-      expect(test_page_preview).to be_on_page
-      expect(entry).not_to be_deleted
-      # Remove the entry created using the Content Management API
-      entry.delete
-      expect(entry).to be_deleted
-    end
-  end
-end
