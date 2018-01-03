df3e6c91c256e8b11c6e68552a95220f26595150
hparekh <hparekh@nd.edu>
2018-01-03 14:51:11 -0500

    Explicitly require Curate::Pages::HomePage
    For some reason the curate_spec_helper won't require all files
    in spec/curate/pages unless I first explicitly require
    `spec/curate/pages/home_page.rb`
    
    Without this change specs fail with an error:
    ``` console
    uninitialized constant Curate::Pages::HomePage
    ```

--- spec/curate/curate_spec_helper.rb
+++ spec/curate/curate_spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'spec_helper'
+require File.expand_path('../pages/home_page.rb', __FILE__)
 Dir.glob(File.expand_path('../pages/**/*.rb', __FILE__)).each do |filename|
   require filename
 end
