de885da0851371566e865d01d97c5b5db6574cb5
hparekh <hparekh@nd.edu>
2018-01-10 09:25:38 -0500

    Updating RBSC functional spec
    * Adds a 'smoke_test' tag for running specs during large changes
    * Adds more assertions to validate homepage

--- spec/rbsc/functional/func_rbsc_spec.rb
+++ spec/rbsc/functional/func_rbsc_spec.rb
@@ -2,11 +2,12 @@
 require 'rbsc/rbsc_spec_helper'
 
 feature 'User Browsing', js: true do
-  scenario 'Load homepage' do
+  scenario 'Load homepage', :smoke_test do
     # Setting js_error false will suppress errors coming back from the site and let the tests finish
     page.driver.browser.js_errors = false
     visit '/'
-    puts current_url
+    expect(page).to have_content 'Rare Books & Special Collections'
+    expect(page).to have_content 'List of Finding Aids'
     within('#main-menu') do
       expect(page).to have_css 'li', count: 6
     end
