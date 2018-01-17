cc62e4a2f026d4a91e323dffd88b4036ccdfda01
hparekh <hparekh@nd.edu>
2018-01-17 15:46:35 -0500

    Adds specs for Buzz application
    * Verifies that buzz server returns a valid JSON
    * The assertions make sure that the values of various keys in
    the JSON response are set correctly.
    * These assertions make sure that the consuming applications are
    able to stream
    * NOTE: This isn't an ideal way to ensure that the server works
    as expected, but is a good start for smoke tests.

---
+++ spec/buzz/functional/func_buzz_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'buzz/buzz_spec_helper'
+
+feature 'Verify a media file', js: true do
+  scenario 'Returns valid JSON', :smoke_test do
+    visit '/v1/media_files/31112b1e-6a1a-4307-8d00-d298a5c0a6ed'
+    response = Buzz::BuzzPlayer.new
+    expect(response).to be_valid_response
+  end
+end
