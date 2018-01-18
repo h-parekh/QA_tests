56eca20549043e9956b7775148c0f6aac1588b56
Max Hallwachs <mhallwac@nd.edu>
2018-01-18 13:21:55 -0500

    Creates BasePage class which other pages will inherit * Creates the base_page.rb file as a template for the other pages in regards to testing status code and the top and bottom banners. * Alters home_page.rb to only test for the actual content on the page as it inherits the top and bottom banner tests from BasePage * Changes tests to use find(_) methods instead of has(_) methods as they are more accurate.

--- spec/seaside/pages/home_page.rb
+++ spec/seaside/pages/home_page.rb
@@ -3,13 +3,13 @@ require 'seaside/seaside_spec_helper'
 
 module Seaside
   module Pages
-    class HomePage
+    class HomePage < BasePage
       include Capybara::DSL
       include CapybaraErrorIntel::DSL
 
       def on_page?
-        on_valid_url? &&
-          status_response_ok? &&
+        super &&
+          on_valid_url? &&
           valid_page_content?
       end
 
@@ -17,24 +17,11 @@ module Seaside
         current_url == Capybara.app_host
       end
 
-      def status_response_ok?
-        status_code.to_s.match(/^20[0,1,6]$/)
-      end
-
       def valid_page_content?
-        within('.page-navigation') do
-          has_link?('About Seaside') &&
-            has_link?('Planning Seaside') &&
-            has_link?('Building Seaside') &&
-            has_link?('Interactive Map') &&
-            has_field?('.q.search-field') &&
-            has_field?('.search-button')
-        end
-
         within('.featured-content') do
-          has_link?(href: '/360/index.html') &&
-            has_link?(href: '/permalink/ARCH-SEASIDE:4008') &&
-            has_link?(href: '/permalink/ARCH-SEASIDE:2')
+          find_link(href: '/360/index.html').visible? &&
+            find_link(href: '/permalink/ARCH-SEASIDE:4008').visible? &&
+            find_link(href: '/permalink/ARCH-SEASIDE:2').visible?
         end
       end
     end
