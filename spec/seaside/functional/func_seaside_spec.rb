ad7821cbd1f8c20168984a699b4add5da8124927
Max Hallwachs <mhallwac@nd.edu>
2018-01-18 14:45:13 -0500

    Creates scenarios for the community and history pages on Seaside * Creates the community_page.rb file to create the CommunityPage class * Creates the history_page.rb file to create the HistoryPage class * Adds the 'Visit About Seaside - Community Page' scenario under 'User Browsing' feature * Adds the 'Visit About Seaside - History Page' scenario under 'User Browsing' feature

--- spec/seaside/functional/func_seaside_spec.rb
+++ spec/seaside/functional/func_seaside_spec.rb
@@ -2,9 +2,25 @@
 require 'seaside/seaside_spec_helper'
 
 feature 'User Browsing', js: true do
-  scenario 'Load Homepage', :read_only do
+  scenario 'Load Homepage', :smoke_test do
     visit '/'
     home_page = Seaside::Pages::HomePage.new
     expect(home_page).to be_on_page
+  end
+
+  scenario 'Visit About Seaside - Community Page', :read_only do
+    visit '/'
+    click_on('About Seaside')
+    find_link('The Community & Building the Portal').trigger('click')
+    community_page = Seaside::Pages::CommunityPage.new
+    expect(community_page).to be_on_page
+  end
+
+  scenario 'Visit About Seaside - History Page', :read_only do
+    visit '/'
+    click_on('About Seaside')
+    find_link(href: '/essays/seaside-history').trigger('click')
+    history_page = Seaside::Pages::HistoryPage.new
+    expect(history_page).to be_on_page
   end
 end
---
+++ spec/seaside/pages/history_page.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'seaside/seaside_spec_helper'
+
+module Seaside
+  module Pages
+    class HistoryPage < BasePage
+      include Capybara::DSL
+      include CapybaraErrorIntel::DSL
+
+      def on_page?
+        super &&
+          valid_page_content? &&
+          on_valid_url?
+      end
+
+      def on_valid_url?
+        current_url == 'https://seaside.library.nd.edu/essays/seaside-history'
+      end
+
+      def valid_page_content?
+        within('.main') do
+          find('.banner.with-subtitle').visible? &&
+            find_link(href: '/essays/visions-of-seaside').visible? &&
+            find_link(href: '/essays/the-artfulness-of-community-making').visible? &&
+            all('figure.image', count: 2)
+        end
+      end
+    end
+  end
+end
