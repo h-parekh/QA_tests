012afe7ba68623eacb1aa3bd0737a7438f747a6e
Max Hallwachs <mhallwac@nd.edu>
2018-01-22 15:47:23 -0500

    Creates the Loads Homepage scenario and corresponding page file * Creates the scenario for loading the homepage with smoke_test tag * Creates the HomePage class to ensure the homepage loads with header, footer, and page content * Updates spec_helper to require page files * Updates func_inquisition_spec to use correct spec_helper call

--- spec/inquisition/functional/func_inquisition_spec.rb
+++ spec/inquisition/functional/func_inquisition_spec.rb
@@ -1,2 +1,11 @@
 # frozen_string_literal: true
-require 'spec_helper'
+require 'inquisition/inquisition_spec_helper'
+
+feature 'User Browsing', js: true do
+  scenario 'Load Homepage', :read_only, :smoke_test do
+    page.driver.browser.js_errors = false
+    visit '/'
+    home_page = Inquisition::Pages::HomePage.new
+    expect(home_page).to be_on_page
+  end
+end
