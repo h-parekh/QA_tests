c8cf6cabc29fdbdbc789c5620b8d5eecb95271fe
hparekh <hparekh@nd.edu>
2018-01-23 12:56:03 -0800

    Adds scenario for searching with NDCatalog
    This scenario is identical to the one searching with Onesearch
    except that there is a step in between to click the NDcatolog tab

--- spec/primo/functional/func_primo_spec.rb
+++ spec/primo/functional/func_primo_spec.rb
@@ -22,4 +22,19 @@ feature 'User browsing', js: true do
     search_results_page = Primo::Pages::SearchResultsPage.new
     expect(search_results_page).to be_on_page
   end
+
+  scenario 'User searches NDCatalog' do
+    page.driver.browser.js_errors = false
+    visit '/'
+    home_page = Primo::Pages::HomePage.new
+    expect(home_page).to be_on_page
+    # This selects the NDcatalog tab
+    click_on('Search materials held by all Notre Dame campus libraries')
+    expect(home_page).to be_ndcatalog_tab_activated
+    search_term = 'Automated Testing'
+    fill_in('search_field', with: search_term)
+    find_button(id: 'goButton').trigger('click')
+    search_results_page = Primo::Pages::SearchResultsPage.new
+    expect(search_results_page).to be_on_page
+  end
 end
