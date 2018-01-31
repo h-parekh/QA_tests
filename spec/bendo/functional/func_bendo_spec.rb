0d2fc8e32e592e6ac2dd1f396ab1e068bddc123e
hparekh <hparekh@nd.edu>
2018-01-31 07:20:39 -0800

    Adds a health check for bendo
    * Endpoint URLs are stored in corpfs
    * Checks if the page loads with a '200 OK' and looks for keyword
    'Bendo' on the page.
    * NOTE: I'm not verifying the version number of bendo in the assertion
    This will be verified once I have a confirmation from devs about
    their preferred method to store version number in the git repo

---
+++ spec/bendo/functional/func_bendo_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'bendo/bendo_spec_helper'
+
+feature 'Load health status page', js: true do
+  scenario 'Check version', :smoke_test do
+    visit '/'
+    health_status_page = Bendo::Pages::HealthStatusPage.new
+    expect(health_status_page).to be_on_page
+  end
+end
