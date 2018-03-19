2a8d4588fd003285d3c5e8a29d51be5aefc97774
Max Hallwachs <mhallwac@nd.edu>
2018-03-19 15:37:36 -0400

    Creates the createImage method for the ImagePage class The method: * has two parameters:   1. access_rights(defaults to nil): used to defined which radio button to check under access controls   2. embargo_date(defaults to nil): allows tests to not include an embargo date to test preventative measures against a blank embargo date field * generically fills in the necessary information to create the file * uses access_rights to choose the desired access control rights (if nil then defaults to open) * checks contributor agreement * then creates the image work
    Purpose: to create an image work which can be used to test manipulating different access rights to ensure functionality

--- spec/curate/pages/image_page.rb
+++ spec/curate/pages/image_page.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module Curate
   module Pages
     class ImagePage
@@ -28,6 +30,26 @@ module Curate
         has_field?("image[rights]")
         has_field?("image[date_created]")
         has_css?("div.control-group.text.optional.image_description")
+      end
+
+      def createTempImage(access_rights: nil, embargo_date: true)
+        fill_in(id: "image_title", with: "foo")
+        fill_in(id: "image_creator", with: "foo")
+        within('#set-access-controls') do
+          if access_rights == 'ndu' || access_rights == 'open' || access_rights == 'restricted'
+            choose(id: 'visibility_' + access_rights)
+          elsif access_rights == 'embargo'
+            choose(id: 'visibility_' + access_rights)
+            if embargo_date
+              fill_in(id: 'image_embargo_release_date', with: Date.today+1)
+            end
+          else
+            choose(id: 'visbility_open')
+          end
+        end
+        find('#accept_contributor_agreement').trigger('click')
+        find('.btn.btn-primary.require-contributor-agreement').trigger('click')
+        sleep(3)
       end
     end
   end
