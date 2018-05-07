be322f0dd964645f51fe0b260b383ed5617ca43f
hparekh <hparekh@nd.edu>
2018-05-07 10:59:46 -0400

    Fixes indentation violations and removes extra line

--- spec/usurper/pages/items_page.rb
+++ spec/usurper/pages/items_page.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Usurper
   module Pages
     # /personal
@@ -8,22 +9,21 @@ module Usurper
 
       def on_page?
         super &&
-        on_valid_url? &&
-        correct_content?
+          on_valid_url? &&
+          correct_content?
       end
 
       def correct_content?
         page.has_link?('My Courses') &&
-        page.has_link?("Log Out") &&
-        find('h1', text: "Items & Requests").visible? &&
-        find('h3', match: :smart, text: 'Checked Out').visible? &&
-        find('h3', match: :smart, text: 'Pending').visible?
+          page.has_link?("Log Out") &&
+          find('h1', text: "Items & Requests").visible? &&
+          find('h3', match: :smart, text: 'Checked Out').visible? &&
+          find('h3', match: :smart, text: 'Pending').visible?
       end
 
       def on_valid_url?
         current_url == File.join(Capybara.app_host, "items-requests") || current_url == File.join(Capybara.app_host, "items-requests#")
       end
-
     end
   end
 end
