5fc4605c8648b0d06969c60eb581656338377866
hparekh <hparekh@nd.edu>
2018-05-07 09:10:50 -0400

    Adds empty lines

--- spec/osf/functional/func_osf_spec.rb
+++ spec/osf/functional/func_osf_spec.rb
@@ -4,6 +4,7 @@ require 'osf/osf_spec_helper'
 
 feature 'Institutional Login', js: true do
   let(:login_page) { LoginPage.new(current_logger, account_details_updated: false) }
+  
   scenario 'Sign in by institution (Notre Dame)', :read_only do
     visit '/'
     click_on('Sign In')
