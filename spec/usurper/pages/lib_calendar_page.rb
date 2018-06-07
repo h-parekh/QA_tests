2b94bc61f71b4a2432a520ddfd8fc249ce350923
hparekh <hparekh@nd.edu>
2018-06-07 11:17:03 -0700

    Updates Lib calendar page with comments and assertions

--- spec/usurper/pages/lib_calendar_page.rb
+++ spec/usurper/pages/lib_calendar_page.rb
@@ -23,11 +23,11 @@ module Usurper
       end
 
       def correct_content?
-        page.has_selector?('.btn.dropdown-toggle.btn-default.btn-sm') &&
-          page.has_selector?('.btn.dropdown-toggle.bs-placeholder.btn-default.btn-sm') &&
-          page.has_selector?('#s-lc-c-dp-m') &&
-          page.has_selector?('#s-lc-c-search') &&
-          page.has_selector?('.input-group-btn')
+        page.has_selector?('.btn.dropdown-toggle.btn-default.btn-sm') && # Calendar library filter  dropdown
+          page.has_selector?('.btn.dropdown-toggle.bs-placeholder.btn-default.btn-sm') && # Category filter dropdown
+          page.has_selector?('#s-lc-c-search') && # Search for event form input
+          page.has_selector?('.input-group-btn') && # Search button
+          page.has_selector?('table.s-lc-mc') # Table of Calendar dates
       end
 
       def correct_url?
