fba2d0166f09ff7256a4555be317d719a1fc1a9a
hparekh <hparekh@nd.edu>
2018-07-30 10:22:46 -0700

    Simplifies methods and scenarios
    * Removes circulatory method calls for `visit_home` and `sign_in`
    from other methods. Instead calling them from scenarios to improve
    readability of scenario
    * Adds a method for `sign_out`
    * Simplifies feature and scenarios for 'first time' and 'Returning'
    users.
    * collapses scenarios for 'Returning users' to reduce the number
    of logins, improves speed of overall excecution

--- spec/sipity/functional/func_sipity_spec.rb
+++ spec/sipity/functional/func_sipity_spec.rb
@@ -8,7 +8,6 @@ def visit_home
 end
 
 def sign_in
-  visit_home
   within('div.collapse.navbar-collapse') do
     find_link('Sign in').click
   end
@@ -16,16 +15,16 @@ def sign_in
   casLogin.complete_login
 end
 
-def returning_user_sign_in
-  sign_in
-  signed_in_page = Sipity::Pages::SignedInPage.new
-  expect(signed_in_page).to be_on_page
+def sign_out
+  find_link("Sign out").click
+  expect(casLogin).to be_on_page
 end
 
-feature 'First Time User', js: true do
+feature 'First time user', js: true do
   let(:casLogin) { LoginPage.new(current_logger, terms_of_service_accepted: false) }
 
-  scenario 'FIRST TIME User Sign In', :validates_login, :read_only, js: true do
+  scenario 'Sign In', :validates_login, :read_only, js: true do
+    visit_home
     sign_in
     welcome_page = Sipity::Pages::ETDWelcomePage.new
     expect(welcome_page).to be_on_page
@@ -35,51 +34,36 @@ feature 'First Time User', js: true do
   end
 end
 
-feature 'User Browsing', js: true do
+feature 'Returning User', js: true do
   let(:casLogin) { LoginPage.new(current_logger, terms_of_service_accepted: true) }
 
-  scenario 'Visit Homepage', :smoke_test, :read_only do
+  scenario 'Browsing dashboard', :smoke_test, :read_only do
     visit_home
-  end
-
-  scenario 'RETURN User Sign In', :validates_login, :read_only, js: true do
-    returning_user_sign_in
-  end
-
-  scenario 'View Dashboard', :read_only, js: true do
-    returning_user_sign_in
+    sign_in
+    signed_in_page = Sipity::Pages::SignedInPage.new
+    expect(signed_in_page).to be_on_page
+    # Tests 'Dashboard'
     find_link("Dashboard").click
     dashboard = Sipity::Pages::Dashboard.new
     expect(dashboard).to be_on_page
-  end
-
-  scenario 'Start an ETD Submission', :read_only, js: true do
-    returning_user_sign_in
-    find_link("Start an ETD Submission").click
-    submission_page = Sipity::Pages::ETDSubmissionPage.new
-    expect(submission_page).to be_on_page
-  end
-
-  scenario 'View Submitted ETDs', :read_only, js: true do
-    returning_user_sign_in
-    find_link("View Submitted ETDs").click
-    submission_page = Sipity::Pages::SubmittedETDPage.new
-    expect(submission_page).to be_on_page
-  end
-
-  scenario 'View New Deposit Page', :read_only, js: true do
-    returning_user_sign_in
-    find_link("Dashboard").click
-    dashboard = Sipity::Pages::Dashboard.new
-    expect(dashboard).to be_on_page
+    # Tests 'New Deposit page'
     find_link('New Deposit').click
     new_deposit_page = Sipity::Pages::NewDepositPage.new
     expect(new_deposit_page).to be_on_page
+    sign_out
   end
 
-  scenario 'Sign Out', :read_only, js: true do
-    returning_user_sign_in
-    find_link("Sign out").click
-    expect(casLogin).to be_on_page
+  scenario 'Browse ETD buttons', :read_only, js: true do
+    visit_home
+    sign_in
+    find_link("Start an ETD Submission").click
+    submission_page = Sipity::Pages::ETDSubmissionPage.new
+    expect(submission_page).to be_on_page
+    page.go_back
+    # Tests 'View Submitted ETDs'
+    find_link("View Submitted ETDs").click
+    submission_page = Sipity::Pages::SubmittedETDPage.new
+    expect(submission_page).to be_on_page
+    sign_out
   end
 end
