290f465d2f6047890dbd5347951e4cd0c016df21
hparekh <hparekh@nd.edu>
2018-08-27 14:09:55 -0700

    Removes status code assertion, not supported by Selenium

--- spec/microfilms/pages/home_page.rb
+++ spec/microfilms/pages/home_page.rb
@@ -8,16 +8,11 @@ module Microfilms
 
       def on_page?
         on_valid_url? &&
-          status_response_ok? &&
           valid_page_content?
       end
 
       def on_valid_url?
         current_url == Capybara.app_host || current_url == File.join(Capybara.app_host, '/')
-      end
-
-      def status_response_ok?
-        status_code == 200
       end
 
       def valid_page_content?
