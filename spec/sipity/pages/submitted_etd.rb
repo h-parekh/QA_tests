8ad4e58e97922b29aaea8c4d005489ee081f60d0
hparekh <hparekh@nd.edu>
2018-08-29 09:21:56 -0700

    Updates curate dropdown menu

--- spec/sipity/pages/submitted_etd.rb
+++ spec/sipity/pages/submitted_etd.rb
@@ -25,7 +25,7 @@ module Sipity
         has_content?("My Works")
         has_content?("My Collections")
         has_content?("Group Administration")
-        has_content?("My Profile")
+        has_content?("My Account")
         has_content?("Log Out")
       end
     end
