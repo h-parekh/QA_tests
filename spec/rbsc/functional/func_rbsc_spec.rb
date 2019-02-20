7250c362d2f3753e40449f8c7bec49e19aa79d4d
hparekh <hparekh@nd.edu>
2019-02-20 15:39:54 -0800

    Updates spec/rbsc for menu items and removes js_errors

--- spec/rbsc/functional/func_rbsc_spec.rb
+++ spec/rbsc/functional/func_rbsc_spec.rb
@@ -4,13 +4,11 @@ require 'rbsc/rbsc_spec_helper'
 
 feature 'User Browsing', js: true do
   scenario 'Load homepage', :smoke_test, :read_only do
-    # Setting js_error false will suppress errors coming back from the site and let the tests finish
-    page.driver.browser.js_errors = false
     visit '/'
     expect(page).to have_content 'Rare Books & Special Collections'
     expect(page).to have_content 'List of Finding Aids'
     within('#main-menu') do
-      expect(page).to have_css 'li', count: 6
+      expect(page).to have_css 'li', count: 5
     end
   end
 end
