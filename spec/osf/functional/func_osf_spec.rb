61d45d2ff7ee3c9bd13f5034174d37a12371d88f
hparekh <hparekh@nd.edu>
2019-02-20 15:34:35 -0800

    Updates selectors for osf spec

--- spec/osf/functional/func_osf_spec.rb
+++ spec/osf/functional/func_osf_spec.rb
@@ -8,14 +8,15 @@ feature 'Institutional Login', js: true do
   scenario 'Sign in by institution (Notre Dame)', :read_only do
     visit '/'
     click_on('Sign In')
-    page.has_selector?('#alternative-institution')
-    find('#alternative-institution').click
+    page.has_selector?('#alt-login-inst')
+    find('#alt-login-inst').click
     find('#institution-form-select').click
     page.has_field?('option', text: 'University of Notre Dame')
     page.select('University of Notre Dame')
-    find('input[name=submit]').trigger(:click)
+    find(:css, '#consent-checkbox').set(true)
+    find('#institution-login').click
     login_page.complete_login
-    expect(page).to have_selector('.dropdown-toggle.nav-user-dropdown')
+    expect(page).to have_button('Create new project')
     # This selector is the user dropdown menu that only appears with a successful login
   end
 end
