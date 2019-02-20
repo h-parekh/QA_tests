0106bb44dc11879736cefa7ef8a7cc45e6fe1f96
hparekh <hparekh@nd.edu>
2019-02-20 14:48:31 -0800

    Merges 3 scenarios into 1

--- spec/dec/functional/func_dec_spec.rb
+++ spec/dec/functional/func_dec_spec.rb
@@ -3,27 +3,14 @@
 require 'dec/dec_spec_helper'
 
 feature 'User Browsing', js: true do
-  scenario 'DEC_1', :read_only, :smoke_test do
+  scenario 'Select a collection', :read_only, :smoke_test do
     visit '/'
     home_page = Dec::Pages::HomePage.new
     expect(home_page).to be_on_page
-  end
-
-  scenario 'DEC_2', :read_only, :smoke_test do
-    visit '/'
-    home_page = Dec::Pages::HomePage.new
-    expect(home_page).to be_on_page
-    home_page.click_collections_page
-    collections_page = Dec::Pages::CollectionsPage.new
-    expect(collections_page).to be_on_page
-  end
-
-  scenario 'DEC_3', :read_only, :smoke_test do
-    visit '/'
-    home_page = Dec::Pages::HomePage.new
     home_page.click_collections_page
     collections_page = Dec::Pages::CollectionsPage.new
     expect(collections_page).to be_on_page
     collections_page.click_forward_arrow
+    expect(page).to have_selector("div", id: "page-show")
   end
 end
