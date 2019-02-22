f0acd0611801d9e0e5f5c0eb1001450119c5b1f9
hparekh <hparekh@nd.edu>
2019-02-22 15:54:39 -0800

    Fixes spec/vatican

--- spec/vatican/pages/instructions_page.rb
+++ spec/vatican/pages/instructions_page.rb
@@ -8,18 +8,13 @@ module Vatican
 
       def on_page?
         on_valid_url? &&
-          status_response_ok? &&
           valid_page_content?
       end
 
       def on_valid_url?
         current_url == File.join(Capybara.app_host, 'using')
       end
-
-      def status_response_ok?
-        status_code == 0 || 200
-      end
-
+      
       def valid_page_content?
         page.has_content?("How To Use the Database")
       end
